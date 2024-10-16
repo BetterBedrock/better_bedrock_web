70ba3c73f66c7a1f42640bbcbdf1bd287a441559
iDarkQ <matikuki3@gmail.com>
2024-10-16 13:24:42 +0100

    refactor: remove console logs

--- src/components/bedrock/BedrockText.tsx
+++ src/components/bedrock/BedrockText.tsx
@@ -10,12 +10,9 @@ const BedrockText: React.FC<BedrockTextProps> = ({
     text,
     font
 }) => {
-    console.log("Before logging font: ", font)
     if (font === undefined) {
         font = "Mojangles";
     }
-
-    console.log("Text: ", text,  " | Font name: ", font)
 
     return (
         <p className="bedrock-text non-selectable" style={{fontFamily: font}}>
