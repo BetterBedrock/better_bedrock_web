66cb4713bcee7e10ce1b0a69199a29bc2c73fb6a
AmBro <axmbro@gmail.com>
2025-02-05 23:38:39 +0100

    feat: add dynamic background colors to Label component based on type prop

--- apps/frontend/src/components/bedrock/label/label.tsx
+++ apps/frontend/src/components/bedrock/label/label.tsx
@@ -5,12 +5,13 @@ interface LabelProps {
   height?: string;
   width: string;
   minHeight?: string;
+  type?: "green" | "white";
 }
 
-export const Label= ({ children, width, height, minHeight }: LabelProps) => {
+export const Label = ({ children, width, height, minHeight, type = "white" }: LabelProps) => {
   return (
-    <div className={styles.outer_div} style={{ width: width, height: height, minHeight: minHeight}}>
-      <div className={styles.inner_div}>
+    <div outer-data-type={type} className={styles.outer_div} style={{ width: width, height: height, minHeight: minHeight }}>
+      <div inner-data-type={type} className={styles.inner_div}>
         {children}
       </div>
     </div>
