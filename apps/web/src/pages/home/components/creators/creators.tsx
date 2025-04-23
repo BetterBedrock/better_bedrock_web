f01943e38794669d27ca28d9d17768c6ad216542
iDarkQ <matikuki3@gmail.com>
2025-04-23 14:35:48 +0100

    refactor(creators.tsx): utilize newly created CreatorsModule to display contributors lists

--- apps/web/src/pages/home/components/creators/creators.tsx
+++ apps/web/src/pages/home/components/creators/creators.tsx
@@ -1,9 +1,17 @@
-import { CreatorsHeader } from "./creators-header";
 import { Section } from "~/components/section";
-import { styles } from ".";
+import { CREATORS, HELPERS, styles, CreatorsModule } from ".";
 
 export const Creators = () => (
   <Section className={styles.background}>
-    <CreatorsHeader />
+    <CreatorsModule
+      title="CREATORS"
+      description="Meet the creators behind the project! Initially created by AmBro, then extended by iDarkQ. See below the parts they are responsible for!"
+      contributors={CREATORS}
+    />
+    <CreatorsModule
+      title="HELPERS"
+      description="Meet the Better Bedrock helpers. These people have significantly contributed to the development of many features in the TexturePack."
+      contributors={HELPERS}
+    />
   </Section>
 );
