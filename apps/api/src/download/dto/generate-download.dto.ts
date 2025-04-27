a7569cab743dc383da41477f1d673a8f795a7d3a
iDarkQ <matikuki3@gmail.com>
2025-04-27 10:35:22 +0100

    feat(dto): create dto's for download endpoint

--- apps/api/src/download/dto/generate-download.dto.ts
+++ apps/api/src/download/dto/generate-download.dto.ts
@@ -1,6 +1,10 @@
 import { IsString } from "class-validator";
 
 export class GenerateDownloadDto {
+    /**
+     * Name of the file user wants to generate download for
+     * @example 'better-bedrock.txt'
+     */
     @IsString()
     file: string;
 }
