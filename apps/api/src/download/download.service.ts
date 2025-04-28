43faa84194f7672785fe6ba41e56b356ce38129c
iDarkQ <matikuki3@gmail.com>
2025-04-28 10:36:13 +0100

    feat(download.service.ts): generate prisma functions

--- apps/api/src/download/download.service.ts
+++ apps/api/src/download/download.service.ts
@@ -1,15 +1,56 @@
 import { Injectable } from "@nestjs/common";
-import { Download } from "src/download/interfaces/download.interface";
+import { PrismaService } from "../prisma.service";
+import { Prisma, Download } from "@prisma/client";
 
 @Injectable()
 export class DownloadService {
-  private readonly downloads: Download[] = [];
+    constructor(private prisma: PrismaService) {}
 
-  download(download: Download) {
-    this.downloads.push(download);
-  }
+    async download(
+        downloadWhereUniqueInput: Prisma.DownloadWhereUniqueInput,
+    ): Promise<Download | null> {
+        return this.prisma.download.findUnique({
+            where: downloadWhereUniqueInput,
+        });
+    }
 
-  fetchAll() {
-    return this.downloads;
-  }
+    async downloads(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.DownloadWhereUniqueInput;
+        where?: Prisma.DownloadWhereInput;
+        orderBy?: Prisma.DownloadOrderByWithRelationInput;
+    }): Promise<Download[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.download.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async createDownload(data: Prisma.DownloadCreateInput): Promise<Download> {
+        return this.prisma.download.create({
+            data,
+        });
+    }
+
+    async updateDownload(params: {
+        where: Prisma.DownloadWhereUniqueInput;
+        data: Prisma.DownloadUpdateInput;
+    }): Promise<Download> {
+        const { where, data } = params;
+        return this.prisma.download.update({
+            data,
+            where,
+        });
+    }
+
+    async deleteDownload(where: Prisma.DownloadWhereUniqueInput): Promise<Download> {
+        return this.prisma.download.delete({
+            where,
+        });
+    }
 }
