c259391b27f09e9232cbbeebe7055be7083ebea7
iDarkQ <matikuki3@gmail.com>
2025-05-02 18:28:42 +0100

    refactor(downloads-main.tsx): make sure to use content from contentProvider and if it has not been fetched yet, display circular progress indicator

--- apps/web/src/pages/downloads/downloads-main.tsx
+++ apps/web/src/pages/downloads/downloads-main.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { MainArchiveButton, MainItemsList } from "./main";
-import { styles, DownloadsHeading, DOWNLOAD_LIST } from ".";
+import { styles, DownloadsHeading } from ".";
+import { useContent } from "~/providers/content";
+import CircularProgressIndicator from "~/components/bedrock/CircularProgressIndicator";
 
 export const DownloadsMain = () => {
+  const { downloads, fetched } = useContent();
   const [showArchived, setShowArchived] = useState(false);
 
-  const archivedCategory = DOWNLOAD_LIST.find((c) => c.title === "Archived");
+  const archivedCategory = downloads.find((c) => c.title === "Archived");
   const visibleCategories = showArchived
-    ? DOWNLOAD_LIST
-    : DOWNLOAD_LIST.filter((c) => c.title !== "Archived");
+    ? downloads
+    : downloads.filter((c) => c.title !== "Archived");
 
+  if (!fetched) {
+    return <CircularProgressIndicator width="50px" height="50px" />;
+  }
   return (
     <>
       {visibleCategories.map((category) => (
