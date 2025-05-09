089ce7a699ac1ba0a7f2524b5891167d4fb22e0f
iDarkQ <matikuki3@gmail.com>
2025-05-09 16:38:35 +0100

    refactor(footer): move footer into a spare folder

---
+++ apps/web/src/components/bedrock/footer/footer.tsx
@@ -0,0 +1,16 @@
+import { Label } from "../label";
+import { BedrockText } from "../text/bedrock-text";
+import { styles } from ".";
+
+export const Footer = () => (
+  <footer>
+    <Label width="auto">
+      <div className={styles.text}>
+        <BedrockText
+          text="Copyright © Better Bedrock | All rights reserved | Not affiliated with Mojang Studios"
+          type="p"
+        />
+      </div>
+    </Label>
+  </footer>
+);
