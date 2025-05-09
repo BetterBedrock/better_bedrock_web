f5af822080a53f962501668d2b6a35e0b7405e13
iDarkQ <matikuki3@gmail.com>
2025-05-09 16:47:30 +0100

    feat(simple-button.tsx): add classname and html attributes to simple button

--- apps/web/src/components/bedrock/simple-button/simple-button.tsx
+++ apps/web/src/components/bedrock/simple-button/simple-button.tsx
@@ -3,8 +3,9 @@ import { useSound } from "use-sound";
 import bedrockClickSound from "~/assets/sounds/minecraft_click.mp3";
 
 import { styles } from ".";
+import clsx from "clsx";
 
-interface SimpleButtonProps {
+interface SimpleButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   height?: number | string;
   width?: number | string;
   children?: ReactNode;
@@ -13,6 +14,7 @@ interface SimpleButtonProps {
   playSound?: boolean;
   tabIndex?: number;
   style?: React.CSSProperties;
+  className?: string;
 }
 
 const SimpleButton = ({
@@ -24,6 +26,8 @@ const SimpleButton = ({
   playSound = false,
   tabIndex,
   style,
+  className,
+  ...props
 }: SimpleButtonProps) => {
   const [_, setClicked] = useState<boolean>(false);
   const [playClickSound] = useSound(bedrockClickSound);
@@ -45,9 +49,10 @@ const SimpleButton = ({
   return (
     <button
       style={{ height, width, ...style }}
-      className={styles.wrapper}
+      className={clsx(styles.wrapper, className && className)}
       onClick={handleClick}
       tabIndex={tabIndex}
+      {...props}
     >
       <div className={styles.child}>{children}</div>
     </button>
