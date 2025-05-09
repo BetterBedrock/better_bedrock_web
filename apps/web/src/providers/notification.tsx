33919401dda3f779d79a6f220acec340881727d6
iDarkQ <matikuki3@gmail.com>
2025-05-09 16:39:40 +0100

    refactor(notification.tsx): finish provider with different types

--- apps/web/src/providers/notification.tsx
+++ apps/web/src/providers/notification.tsx
@@ -1,9 +1,13 @@
-import { randomUUID } from "crypto";
 import { createContext, ReactNode, useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Caution from "~/assets/images/glyphs/Caution.png";
+import Error from "~/assets/images/glyphs/ErrorGlyph.png";
+import Info from "~/assets/images/glyphs/blue_info_glyph.png";
+import Success from "~/assets/images/glyphs/update.png";
 
 interface NotificationContextProps {
   notificationQueue: Notification[];
-  sendNotification: (title: string, label: string, type: "green" | "white") => void;
+  sendNotification: (notification: NotificationInput) => void;
   closeNotification: (id: string) => void;
 }
 
@@ -11,26 +15,67 @@ interface NotificationProviderProps {
   children: ReactNode;
 }
 
+export const NotificationTypes = {
+  warning: {
+    type: "warning",
+    color: "#FF9800",
+    icon: Caution,
+  },
+  success: {
+    type: "success",
+    color: "#4CAF50",
+    icon: Success,
+  },
+  info: {
+    type: "info",
+    color: "#2196F3",
+    icon: Info,
+  },
+  error: {
+    type: "error",
+    color: "#F44336",
+    icon: Error,
+  },
+} as const;
+
+export type NotificationType = keyof typeof NotificationTypes;
+
 export interface Notification {
-  id: string;
+  id?: string;
   title: string;
-  type: "green" | "white";
+  type: NotificationType;
   label: string;
+  time: "short" | "medium" | "long" | "infinite";
 }
+
+type NotificationInput = Omit<Notification, "id" | "time" | "type"> & {
+  id?: string;
+  time?: "short" | "medium" | "long" | "infinite";
+  type?: NotificationType;
+};
 
 const NotificationContext = createContext<NotificationContextProps | undefined>(undefined);
 
 export const NotificationProvider = ({ children }: NotificationProviderProps) => {
   const [notificationQueue, setNotificationQueue] = useState<Notification[]>([]);
 
-  const sendNotification = (title: string, label: string, type: "green" | "white") => {
-    const notification = {
+  const sendNotification = ({
+    title,
+    label,
+    id = uuidv4(),
+    type = "info",
+    time = "short",
+  }: NotificationInput) => {
+    setNotificationQueue((prev) => [
+      ...prev,
+      {
+        id,
         label,
         title,
-        id: randomUUID(),
         type,
-    } as Notification;
-    setNotificationQueue((prev) => [...prev, notification]);
+        time,
+      },
+    ]);
   };
 
   const closeNotification = (id: string) => {
