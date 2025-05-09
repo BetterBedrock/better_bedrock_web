351199803ff10e8bad42ec932fc3ab1b9f31bd50
iDarkQ <matikuki3@gmail.com>
2025-05-09 16:47:00 +0100

    refactor(notification): finish layout for notification

--- apps/web/src/components/bedrock/notification/notification.tsx
+++ apps/web/src/components/bedrock/notification/notification.tsx
@@ -1,8 +1,12 @@
-import { Label } from "~/components/bedrock/label";
 import { styles } from ".";
 import { BedrockText } from "~/components/bedrock/text";
-import { Notification as BaseNotification, useNotification } from "~/providers/notification";
-import SimpleButton from "~/components/bedrock/simple-button/simple-button";
+import {
+  Notification as BaseNotification,
+  NotificationTypes,
+  useNotification,
+} from "~/providers/notification";
+import clsx from "clsx";
+import Exit from "~/assets/images/exit.png";
 
 interface NotificationProps {
   notification: BaseNotification;
@@ -10,18 +14,27 @@ interface NotificationProps {
 
 export const Notification = ({ notification }: NotificationProps) => {
   const { closeNotification } = useNotification();
+  const { color, icon } = NotificationTypes[notification.type];
+
+  const handleExit = () => {
+    closeNotification(notification.id!);
+  };
+
   return (
-    <div className={styles.notification}>
-      <Label type="green" width="300px" height="60px">
-        <BedrockText text={notification.label} type="p" color="white" />
-        <SimpleButton
-          onTap={() => {
-            closeNotification(notification.id);
-          }}
-        >
-          Close
-        </SimpleButton>
-      </Label>
+    <div className={clsx(styles.wrapper, styles.outerShape, styles.outer)}>
+      <div className={clsx(styles.innerShape, styles.inner)}>
+        <div className={styles.column}>
+          <img alt={`${notification.type} notification`} src={icon} className={styles.icon} />
+        </div>
+
+        <div className={styles.column}>
+          <div className={styles.heading}>
+            <BedrockText text={notification.title} type="p" color={color} textAlign="left" />
+            <img alt="Close" src={Exit} className={styles.close} onClick={handleExit} />
+          </div>
+          <BedrockText text={notification.label} type="p" color="white" textAlign="left" />
+        </div>
+      </div>
     </div>
   );
 };
