0357cef84c07a74568ab81eb198898edf4c367de
iDarkQ <matikuki3@gmail.com>
2025-05-10 16:58:21 +0100

    refactor(header): make buttons adjust based on screen size

--- apps/web/src/pages/home/components/hero/header/header-actions.tsx
+++ apps/web/src/pages/home/components/hero/header/header-actions.tsx
@@ -2,24 +2,28 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "~/components/bedrock/button";
 import { styles } from ".";
 import { Routes } from "~/utils/routes";
+import { ButtonSeparator } from "~/components/bedrock/button-separator";
 
 export const HeroActions = () => {
   const navigate = useNavigate();
   return (
     <div className={styles.actions}>
-      <Button
-        text="Download Now"
-        width="100%"
-        type="alwaysGreen"
-        onTap={() => navigate(Routes.DOWNLOADS)}
-      />
-      <Button
-        text="Join Discord"
-        width="100%"
-        type="alwaysWhite"
-        outlinePaddingLeft="0px"
-        onTap={() => window.open("https://discord.gg/ZGK5WYXnEY", "_blank", "noopener,noreferrer")}
-      />
+      <ButtonSeparator>
+        <Button
+          text="Download Now"
+          width="100%"
+          type="alwaysGreen"
+          onTap={() => navigate(Routes.DOWNLOADS)}
+        />
+        <Button
+          text="Join Discord"
+          width="100%"
+          type="alwaysWhite"
+          onTap={() =>
+            window.open("https://discord.gg/ZGK5WYXnEY", "_blank", "noopener,noreferrer")
+          }
+        />
+      </ButtonSeparator>
     </div>
   );
 };
