3bf44daf631d2766a8f57d4892810a090908cb4a
iDarkQ <matikuki3@gmail.com>
2025-05-13 14:28:51 +0100

    style(creators-card.tsx): simplify code

--- apps/web/src/components/bedrock/creators-card/creators-card.tsx
+++ apps/web/src/components/bedrock/creators-card/creators-card.tsx
@@ -1,8 +1,8 @@
 import { Label } from "../label";
-import { BedrockText } from "../text/bedrock-text";
+import { BedrockText } from "../bedrock-text";
 import ExampleHead from "~/assets/images/example_head.png";
 
-import styles from "./creators-card.module.css";
+import { styles } from ".";
 
 interface CreatorCardProp {
   name?: string;
@@ -11,27 +11,15 @@ interface CreatorCardProp {
   height: string;
 }
 
-export const CreatorCard = ({
-  name,
-  description,
-  width,
-  height,
-}: CreatorCardProp) => (
+export const CreatorCard = ({ name, description, width, height }: CreatorCardProp) => (
   <Label height={height} width={width}>
     <div className={styles.content}>
       <div className={styles.image}>
         <img alt="Minecraft Profile Head" src={ExampleHead} />
       </div>
-      <div className={styles.card_description}>
-        <BedrockText
-          text={name ?? "iDarkQ"}
-          type={"h2"}
-          font="MinecraftTen"
-        ></BedrockText>
-        <BedrockText
-          text={description?.join(", ") ?? ""}
-          type={"p"}
-        ></BedrockText>
+      <div className={styles.description}>
+        <BedrockText text={name ?? "iDarkQ"} type="h2" font="MinecraftTen" />
+        <BedrockText text={description?.join(", ") ?? ""} type="p" />
       </div>
     </div>
   </Label>
