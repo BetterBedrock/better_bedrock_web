e16ce53ce58a60693480edc3f4690b33aeae779d
iDarkQ <matikuki3@gmail.com>
2025-05-26 10:45:09 +0100

    refactor(tabs.tsx): utilize button group instead of button seperator

--- apps/web/src/pages/downloads/components/tabs/tabs.tsx
+++ apps/web/src/pages/downloads/components/tabs/tabs.tsx
@@ -3,6 +3,7 @@ import { ButtonSeparator } from "~/components/bedrock/button-separator";
 import { TAB_NAMES } from "../..";
 import { styles } from ".";
 import { useContent } from "~/providers/content";
+import { ButtonGroup } from "~/components/button-group/button-group";
 
 interface TabsProps {
   activeTab: number;
@@ -18,7 +19,7 @@ export const Tabs = ({ activeTab, setActiveTab }: TabsProps) => {
 
   return (
     <div className={styles.tabs}>
-      <ButtonSeparator>
+      <ButtonGroup>
         {TAB_NAMES.map((text, index) => (
           <Button
             key={index}
@@ -31,7 +32,7 @@ export const Tabs = ({ activeTab, setActiveTab }: TabsProps) => {
             type="alwaysBlack"
           />
         ))}
-      </ButtonSeparator>
+      </ButtonGroup>
     </div>
   );
 };
