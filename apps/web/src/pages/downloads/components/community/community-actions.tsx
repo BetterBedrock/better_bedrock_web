c3307890f509394d86162ee7373d1bdcd62b28ea
iDarkQ <matikuki3@gmail.com>
2025-05-30 10:57:46 +0100

    refactor(community-actions.tsx): display real data from buttons provided by api & add display for notifications

--- apps/web/src/pages/downloads/components/community/community-actions.tsx
+++ apps/web/src/pages/downloads/components/community/community-actions.tsx
@@ -2,24 +2,44 @@ import { Button } from "~/components/bedrock/button";
 import { ButtonSeparator } from "~/components/bedrock/button-separator";
 import { styles } from ".";
 import { useNotification } from "~/providers/notification";
+import { DownloadButtonDto } from "@better-bedrock/constants/downloads.dto";
 
-export const CommunityActions = () => {
-  const {sendNotification} = useNotification();
+interface CommunityActionsProps {
+  buttons: DownloadButtonDto[];
+}
+
+export const CommunityActions = ({ buttons }: CommunityActionsProps) => {
+  const { sendNotification } = useNotification();
+
+  const buttonList = buttons?.map((button) => (
+    <Button
+      outlinePaddingRight="0"
+      text={button.text}
+      type={button.type}
+      width="100%"
+      onClick={() => {
+        if (button.link) {
+          window.open(button.link, "_blank", "noopener,noreferrer");
+        }
+
+        if (button.notification) {
+          sendNotification({
+            title: button.notification.title,
+            label: button.notification.description,
+            type: button.notification.type,
+          });
+        }
+      }}
+    />
+  ));
+
   return (
-    <ButtonSeparator className={styles.actions}>
-      <Button outlinePaddingRight="0" text="Submit YOUR texturepack" type="alwaysGreen" width="100%" onClick={() => {
-        sendNotification({
-          title: "Texturepack submission",
-          label: "Please submit your texturepack to our Discord server on #submissions.",
-          type: "info",
-        });
-      }} />
-      <Button
-        text="Watch theme creation tutorial"
-        width="100%"
-        type="alwaysWhite"
-        onTap={() => window.open("https://youtu.be/GRQahMrdEoY", "_blank", "noopener,noreferrer")}
-      />
-    </ButtonSeparator>
+    <>
+      {buttonList.length < 2 ? (
+        buttonList
+      ) : (
+        <ButtonSeparator className={styles.actions}>{...buttonList}</ButtonSeparator>
+      )}
+    </>
   );
 };
