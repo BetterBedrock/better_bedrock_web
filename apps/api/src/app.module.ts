61ef2de1433475b02bc097776656a7a3c1824b58
iDarkQ <matikuki3@gmail.com>
2025-06-01 07:54:30 +0100

    feat(app.module.ts): add checkout module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { DownloadModule } from "src/download/download.module";
 import { ContentModule } from "./content/content.module";
+import { CheckoutModule } from "./checkout/checkout.module";
 
 @Module({
-    imports: [DownloadModule, ContentModule],
+    imports: [DownloadModule, ContentModule, CheckoutModule],
 })
 export class AppModule {}
