d2d22a12b6ac7bcdc373fae30237080ab17454bc
iDarkQ <matikuki3@gmail.com>
2025-06-07 20:55:31 +0100

    feat(checkout.module.ts): add voucher and prisma services

--- apps/api/src/checkout/checkout.module.ts
+++ apps/api/src/checkout/checkout.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { CheckoutController } from "src/checkout/checkout.controller";
 import { CheckoutService } from "./checkout.service";
+import { VoucherService } from "src/voucher/voucher.service";
+import { PrismaService } from "src/prisma.service";
 
 @Module({
     controllers: [CheckoutController],
-    providers: [CheckoutService],
+    providers: [CheckoutService, VoucherService, PrismaService],
 })
 export class CheckoutModule {}
