b1b83b8b1ed4158baef9b8dbdfa365074bbd3af2
iDarkQ <matikuki3@gmail.com>
2025-06-07 20:55:12 +0100

    feat(checkout.controller.ts): create offers, activate endpoints & finish create with webhook

--- apps/api/src/checkout/checkout.controller.ts
+++ apps/api/src/checkout/checkout.controller.ts
@@ -6,26 +6,99 @@ import {
     Post,
     RawBodyRequest,
     Req,
+    Get,
+    Query,
+    BadGatewayException,
+    NotFoundException,
+    BadRequestException,
 } from "@nestjs/common";
-import { ApiOkResponse } from "@nestjs/swagger";
 import { CheckoutService } from "src/checkout/checkout.service";
+import { ActivateVoucherDto } from "src/checkout/dto/activate-voucher.dto";
+import { VoucherService } from "src/voucher/voucher.service";
 import Stripe from "stripe";
+import {
+    ApiBadGatewayResponse,
+    ApiBadRequestResponse,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiTags,
+} from "@nestjs/swagger";
+import { CreateCheckoutSessionDto } from "src/checkout/dto/create-checkout-session.dto";
+import { CHECKOUT_OFFERS } from "src/checkout/constants/checkout-offers";
+import { CheckoutOffersDto } from "@better-bedrock/constants/checkout.dto";
+import { VoucherDto } from "@better-bedrock/constants/voucher.dto";
+import { CreateCheckoutSessionResponseDto } from "src/checkout/dto/create-checkout-session-response.dto";
+import { obscureEmail } from "src/utils/string";
 
+@ApiTags("checkout")
 @Controller("checkout")
 export class CheckoutController {
-    constructor(private readonly checkoutService: CheckoutService) {}
+    constructor(
+        private readonly checkoutService: CheckoutService,
+        private readonly voucherService: VoucherService,
+    ) {}
+
+    @Get("offers")
+    @ApiOkResponse({
+        description: "Returns current checkout offers",
+        type: CheckoutOffersDto,
+    })
+    async offers() {
+        return CHECKOUT_OFFERS;
+    }
 
     @Post("create")
-    @ApiOkResponse({ description: "Create a checkout session" })
-    async create() {
+    @ApiOkResponse({
+        description: "Create a checkout session",
+        type: CreateCheckoutSessionResponseDto,
+    })
+    @ApiBadGatewayResponse({ description: "Checkout could not be initiated" })
+    @ApiNotFoundResponse({ description: "This offer does not exist" })
+    async create(
+        @Query() query: CreateCheckoutSessionDto,
+    ): Promise<CreateCheckoutSessionResponseDto> {
+        const ids = CHECKOUT_OFFERS.offers.flatMap((offer) => offer.items);
+        if (!ids.find((item) => item.priceId === query.priceId)) {
+            throw new NotFoundException("This offer does not exist");
+        }
         try {
-            const id = await this.checkoutService.createCheckoutSession();
-            return { sessionId: id };
-        } catch (error) {
-            console.error("Error creating checkout session:", error);
+            const checkoutId = await this.checkoutService.createCheckoutSession(query);
+            return { checkoutId };
+        } catch (err) {
+            throw new BadGatewayException("Checkout could not be initiated: " + err);
+        }
+    }
+
+    @Get("activate")
+    @ApiOkResponse({
+        description: "Returns voucher for given code",
+        type: VoucherDto,
+    })
+    @ApiNotFoundResponse({ description: "Voucher not found" })
+    @ApiBadRequestResponse({ description: "You need to provide either checkoutId or voucher code" })
+    @ApiBadGatewayResponse({ description: "Activation was unsuccessful" })
+    async activate(@Query() query: ActivateVoucherDto) {
+        if (!query.checkoutId && !query.code) {
+            throw new BadRequestException("You need to provide either checkoutId or voucher code");
         }
 
-        return {};
+        try {
+            Logger.debug({ checkoutId: query.checkoutId, code: query.code });
+            const voucher = await this.voucherService.getVoucher({
+                checkoutId: query.checkoutId,
+                code: query.code,
+            });
+            if (!voucher) {
+                throw new NotFoundException("Voucher not found");
+            }
+
+            voucher.email = obscureEmail(voucher.email);
+
+            return voucher;
+        } catch (err) {
+            if (err instanceof HttpException) throw err;
+            throw new BadGatewayException("Activation was unsuccessful" + err);
+        }
     }
 
     @Post("webhook")
@@ -34,7 +107,6 @@ export class CheckoutController {
         @Headers("stripe-signature") signature: string,
         @Req() req: RawBodyRequest<Request>,
     ) {
-        Logger.error("Received webhook event:");
         let event;
         try {
             event = this.checkoutService.constructWebhookEvent(req.rawBody!, signature);
@@ -44,21 +116,39 @@ export class CheckoutController {
         }
 
         if (event.type === "checkout.session.completed") {
-            Logger.debug(event.data.object);
             const session = event.data.object as Stripe.Checkout.Session;
             const fullSession = await this.checkoutService.retriveSession(session.id);
 
             const email = fullSession.customer_details?.email;
             const items = fullSession.line_items!.data;
-
             items.forEach((item) => {
                 const productName = (item!.price!.product as Stripe.Product).name;
+                const priceId = item!.price!.id;
+                const ids = CHECKOUT_OFFERS.offers.flatMap((offer) => offer.items);
+                const product = ids.find((item) => item.priceId === priceId);
+                Logger.error("test 3");
+
+                if (!product) {
+                    Logger.error(
+                        `User with email ${email} tried to purchase ${priceId} but the product does not exist`,
+                    );
+                    return;
+                }
+
+                const priceOptions = product.priceOption;
+
                 const price = item.amount_total / 100;
                 // reward user
                 Logger.log(`User with email ${email} purchased ${productName} for $${price}.`);
+                this.voucherService.createVoucher({
+                    email: email ?? "unknown",
+                    checkoutId: session.id,
+                    code: this.voucherService.generateCode(8),
+                    maxDownloads: priceOptions.maxDownloads,
+                    betterBedrockContentOnly: priceOptions.betterBedrockContentOnly,
+                    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * priceOptions.expiresAt), // 30 days from now
+                });
             });
         }
-
-        return { received: true };
     }
 }
