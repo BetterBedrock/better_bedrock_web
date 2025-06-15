338ac13c35b4569d13a47c49a65399dce1e32a9a
iDarkQ <matikuki3@gmail.com>
2025-06-15 20:35:35 +0100

    refactor(popup): add minecraft header

--- apps/web/src/components/bedrock/popup/popup.tsx
+++ apps/web/src/components/bedrock/popup/popup.tsx
@@ -2,14 +2,16 @@ import { Card } from "~/components/bedrock/card";
 import { styles } from ".";
 import clsx from "clsx";
 import Exit from "~/assets/images/exit.png";
+import { BedrockText } from "~/components/bedrock/bedrock-text";
 
 interface PopupProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   className?: string;
+  title?: string;
   onClose?: () => void;
 }
 
-export const Popup = ({ children, onClose, className }: PopupProps) => (
+export const Popup = ({ children, onClose, className, title }: PopupProps) => (
   <div className={styles.popup} onClick={onClose}>
     <Card
       className={clsx(styles.body, className && className)}
@@ -17,6 +19,9 @@ export const Popup = ({ children, onClose, className }: PopupProps) => (
     >
       <div className={styles.header}>
         <img alt="Close" src={Exit} className={styles.close} onClick={onClose} />
+        {title && (
+          <BedrockText text={title} font="Minecraft" type="h2" color="white" textAlign="center" />
+        )}
       </div>
       {children}
     </Card>
