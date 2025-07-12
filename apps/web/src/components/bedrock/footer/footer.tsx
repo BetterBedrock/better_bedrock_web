d5850cfdfbf5e692e5e9798081d5a95bbe85deaf
iDarkQ <matikuki3@gmail.com>
2025-07-12 08:12:04 +0100

    refactor(footer.tsx): utilize link component

--- apps/web/src/components/bedrock/footer/footer.tsx
+++ apps/web/src/components/bedrock/footer/footer.tsx
@@ -2,26 +2,20 @@ import { Label } from "../label";
 import { BedrockText } from "../bedrock-text/bedrock-text";
 import { styles } from ".";
 import { Routes } from "~/utils/routes";
+import { Link } from "~/components/link";
 
 export const Footer = () => {
-
   return (
     <footer>
       <Label width="auto">
         <div className={styles.footer}>
           <div className={styles.links}>
-            <BedrockText
-              text="Privacy Policy"
-              type="p"
-              link={Routes.PRIVACY_POLICY}
-              isExternalLink={false}
-            />
-            <BedrockText
-              text="GitHub"
-              type="p"
-              link="https://github.com/BetterBedrock/better_bedrock_web"
-              isExternalLink={true}
-            />
+            <Link link={Routes.PRIVACY_POLICY}>
+              <BedrockText text="Privacy Policy" type="p" />
+            </Link>
+            <Link isExternalLink={true} link="https://github.com/BetterBedrock/better_bedrock_web">
+              <BedrockText text="GitHub" type="p" />
+            </Link>
           </div>
           <BedrockText
             text="Copyright © Better Bedrock | All rights reserved | Not affiliated with Mojang Studios"
