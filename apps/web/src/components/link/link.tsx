d934c4d79a9d961557fc39c28d57711de4f6508f
iDarkQ <matikuki3@gmail.com>
2025-07-13 17:54:38 +0100

    refactor(link): add option to hide styles

--- apps/web/src/components/link/link.tsx
+++ apps/web/src/components/link/link.tsx
@@ -1,20 +1,34 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
+import { styles } from ".";
 
 interface LinkProps {
   link: string;
   isExternalLink?: boolean;
   children: ReactNode;
+  hideStyles?: boolean;
   className?: string;
 }
 
-export const Link = ({ children, link, isExternalLink = false, className }: LinkProps) => {
+export const Link = ({
+  children,
+  hideStyles = false,
+  link,
+  isExternalLink = false,
+  className,
+}: LinkProps) => {
   return isExternalLink ? (
-    <a href={link} className={className && className} target="_blank" rel="noopener noreferrer">
+    <a
+      href={link}
+      className={clsx(className && className, hideStyles && styles.hide)}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   ) : (
-    <ReactRouterLink to={link} className={className && className}>
+    <ReactRouterLink to={link} className={clsx(className && className, hideStyles && styles.hide)}>
       {children}
     </ReactRouterLink>
   );
