04ece4a3a01f4546f00f285655a92b70b4c20a08
iDarkQ <matikuki3@gmail.com>
2025-07-14 18:09:54 +0100

    refactor(changelog-actions.tsx): replace button seperator with button group and remove wrapper div

--- apps/web/src/pages/home/components/changelog/changelog-actions.tsx
+++ apps/web/src/pages/home/components/changelog/changelog-actions.tsx
@@ -2,11 +2,12 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "~/components/bedrock/button";
 import { styles } from ".";
 import { Routes } from "~/utils/routes";
+import { ButtonGroup } from "~/components/button-group/button-group";
 
 export const ChangelogActions = () => {
   const navigate = useNavigate();
   return (
-    <div className={styles.actions}>
+    <ButtonGroup className={styles.actions}>
       <Button
         text="Information Page"
         width="100%"
@@ -21,6 +22,6 @@ export const ChangelogActions = () => {
         type="alwaysWhite"
         onTap={() => window.open("https://discord.gg/ZGK5WYXnEY", "_blank", "noopener,noreferrer")}
       />
-    </div>
+    </ButtonGroup>
   );
 };
