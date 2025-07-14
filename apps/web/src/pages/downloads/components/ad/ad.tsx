89a879c616155582556b4a6e401a60f09ccc0c2f
iDarkQ <matikuki3@gmail.com>
2025-07-14 17:57:37 +0100

    refactor(ad.tsx): hide current ad

--- apps/web/src/pages/downloads/components/ad/ad.tsx
+++ apps/web/src/pages/downloads/components/ad/ad.tsx
@@ -1,8 +1,11 @@
-import { AdBanner, AdLabel } from ".";
+// import { AdBanner, AdLabel } from ".";
+
+// This component is currently commented out. Revert it once the advertiser is ready.
+// Uncomment the lines below to enable the ad functionality.
 
 export const Ad = () => (
   <>
-    <AdLabel />
-    <AdBanner />
+    {/* <AdLabel />
+    <AdBanner /> */}
   </>
 );
