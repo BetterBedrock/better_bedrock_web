b26a0b17cdc6044243f6fa9831619bb16f7d68ba
iDarkQ <matikuki3@gmail.com>
2025-07-18 21:35:04 +0100

    feat(auth): create auth module && admin auth guard

---
+++ apps/api/src/auth/admin-auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    InternalServerErrorException,
+    UnauthorizedException,
+} from "@nestjs/common";
+
+import { Request } from "express";
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+
+        const secret = process.env.ADMIN_PANEL_SECRET;
+
+        if (!secret || secret == "") {
+            throw new InternalServerErrorException("Secret for admin panel is not set");
+        }
+
+        if (!token || token !== process.env.ADMIN_PANEL_SECRET) {
+            throw new UnauthorizedException("Could not authenticate");
+        }
+
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(" ") ?? [];
+        return type === "Bearer" ? token : undefined;
+    }
+}
