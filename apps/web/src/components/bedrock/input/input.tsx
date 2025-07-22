1ec4fae76a9b55c6476c6bdcdf828316ad090b3d
iDarkQ <matikuki3@gmail.com>
2025-07-22 21:20:51 +0100

    refactor(input): finish input switch

--- apps/web/src/components/bedrock/input/input.tsx
+++ apps/web/src/components/bedrock/input/input.tsx
@@ -1,6 +1,7 @@
 import { InputHTMLAttributes } from "react";
 import { styles } from ".";
 import clsx from "clsx";
+import { InputSwitch } from "~/components/bedrock/input/input-switch";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
@@ -9,13 +10,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 
-export const Input = ({ placeholder = "Enter text", value, onChange, className, ...props }: InputProps) => (
-  <input
+export const Input = ({
+  placeholder = "Enter text",
+  value,
+  onChange,
+  className,
+  ...props
+}: InputProps) => {
+  if (props.type === "checkbox") {
+    return <InputSwitch value={value} onChange={onChange} className={className} {...props} />;
+  }
+
+  return <input
     value={value}
     onChange={onChange}
-    type="text"
     className={clsx(styles.input, className && className)}
     placeholder={placeholder}
     {...props}
-  />
-);
+  />;
+};
