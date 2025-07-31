5b531eb6e2862e34ce9796b7d1d651c6a53e5a55
iDarkQ <matikuki3@gmail.com>
2025-07-31 21:58:34 +0100

    feat(statistics): add test visits data chart

--- apps/web/src/pages/panel/analytics/components/hero/statistics/statistics.tsx
+++ apps/web/src/pages/panel/analytics/components/hero/statistics/statistics.tsx
@@ -3,6 +3,13 @@ import { styles } from ".";
 
 export const Statistics = () => (
   <div className={styles.list}>
+    {/* <Label extraClassName={styles.label}>
+      <div className={styles.data}>
+        <BedrockText type="p" text={"Visits"} textAlign="left" color="grey" />
+        <BedrockText type="h2" text={"100021"} textAlign="left" font="Minecraft" />
+      </div>
+      <BarChart direction={"horizontal"} data={undefined} />
+    </Label> */}
     <StatisticsCard name="Total Downloads" data="221423" className={styles.card} />
     <StatisticsCard name="Verified Downloads" data="123445" className={styles.card} />
     <StatisticsCard name="Downloads Today" data="121" className={styles.card} />
