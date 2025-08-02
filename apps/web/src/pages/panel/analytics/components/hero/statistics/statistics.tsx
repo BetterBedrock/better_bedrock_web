20fedc1426300d2543ceae7bef96be3f657aee3d
iDarkQ <matikuki3@gmail.com>
2025-08-02 10:28:29 +0100

    refactor(analytics): display all data from the server

--- apps/web/src/pages/panel/analytics/components/hero/statistics/statistics.tsx
+++ apps/web/src/pages/panel/analytics/components/hero/statistics/statistics.tsx
@@ -1,18 +1,99 @@
+// import { StatisticsCard } from "~/components/bedrock/statistics-card";
 import { StatisticsCard } from "~/components/bedrock/statistics-card";
 import { styles } from ".";
+import { useAnalytics } from "~/providers/analytics";
+import { useVoucher } from "~/providers/voucher";
+import dayjs from "dayjs";
+import { AnalyticsNames } from "~/lib/api";
 
-export const Statistics = () => (
-  <div className={styles.list}>
-    {/* <Label extraClassName={styles.label}>
-      <div className={styles.data}>
-        <BedrockText type="p" text={"Visits"} textAlign="left" color="grey" />
-        <BedrockText type="h2" text={"100021"} textAlign="left" font="Minecraft" />
+export const Statistics = () => {
+  const { analytics } = useAnalytics();
+  const { vouchers } = useVoucher();
+
+  const categories = analytics
+    .filter((value) => value.type === "general")
+    .map((value) => value.name);
+
+  const data = categories.reduce((acc: { [key: string]: typeof analytics }, category) => {
+    acc[category] = analytics.filter((a) => a.name === category);
+    return acc;
+  }, {});
+
+  console.log({ keys: Object.keys(data) });
+
+  const simplifiedData = Object.keys(data).reduce((acc: { [key: string]: number }, key) => {
+    acc[key] = data[key].reduce((sum, p) => sum + p.value, 0) || 0;
+    return acc;
+  }, {});
+
+  console.log({ simplifiedData });
+  const averageVoucherUse =
+    vouchers.reduce((sum, cur) => {
+      return sum + cur.downloadCount;
+    }, 0) / vouchers.length;
+
+  const averageVoucherUsePercentage =
+    vouchers.reduce((sum, cur) => {
+      return sum + (cur.downloadCount / cur.maxDownloads) * 100;
+    }, 0) / vouchers.length;
+
+  const validVouchers = vouchers.filter(
+    (voucher) =>
+      dayjs().isBefore(voucher.expiresAt) && voucher.downloadCount < voucher.maxDownloads,
+  ).length;
+
+  const usedVouchers = vouchers.length - validVouchers;
+
+  console.log({ averageVoucherUse, averageVoucherUsePercentage });
+  // const averageVoucherUsePercentage = vouchers.reduce((sum, cur, index) => sum + cur.downloadCount)
+  const estimatedProfit = ((simplifiedData["Ad Downloads"] ?? 0) / 1000) * 9;
+
+  console.log({ estimatedProfit });
+  return (
+    <div className={styles.data}>
+      <StatisticsCard name={AnalyticsNames.Visits} data={data[AnalyticsNames.Visits]} className={styles.card} />
+      <StatisticsCard
+        name="Ads Estimated Revenue"
+        data={estimatedProfit}
+        suffix="$"
+        showGraph={false}
+        className={styles.card}
+      />
+      <div className={styles.list}>
+        <StatisticsCard
+          name="Total Downloads"
+          data={data[AnalyticsNames.TotalDownloads]}
+          className={styles.card}
+        />
+        <StatisticsCard
+          name="Generated Downloads"
+          data={data[AnalyticsNames.GeneratedDownloads]}
+          className={styles.card}
+        />
+        <StatisticsCard name="Ad Downloads" data={data[AnalyticsNames.AdDownloads]} className={styles.card} />
+        <StatisticsCard
+          name="Voucher Downloads"
+          data={data[AnalyticsNames.VoucherDownloads]}
+          className={styles.card}
+        />
+        <StatisticsCard
+          name="Average Voucher Use"
+          data={averageVoucherUse}
+          className={styles.card}
+        />
+        <StatisticsCard
+          name="Average Voucher Use %"
+          data={averageVoucherUsePercentage}
+          suffix="%"
+          className={styles.card}
+        />
+        <StatisticsCard name="Valid Vouchers" data={validVouchers} className={styles.card} />
+        <StatisticsCard
+          name="Used Vouchers"
+          data={usedVouchers}
+          className={styles.card}
+        />
       </div>
-      <BarChart direction={"horizontal"} data={undefined} />
-    </Label> */}
-    <StatisticsCard name="Total Downloads" data="221423" className={styles.card} />
-    <StatisticsCard name="Verified Downloads" data="123445" className={styles.card} />
-    <StatisticsCard name="Downloads Today" data="121" className={styles.card} />
-    <StatisticsCard name="Downloads This Week" data="879" className={styles.card} />
-  </div>
-);
+    </div>
+  );
+};
