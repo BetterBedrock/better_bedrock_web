da8deb0a1b5234385b1f7f503d578a5a3cadff3b
iDarkQ <matikuki3@gmail.com>
2025-08-03 09:41:45 +0100

    feat(analytics-names.ts): create a global constants with analytics names

---
+++ apps/api/src/analytics/constants/analytics-names.ts
@@ -0,0 +1,8 @@
+export enum AnalyticsNames {
+    boughtVouchers = "Bought Vouchers",
+    visits = "Visits",
+    totalDownloads = "Total Downloads",
+    voucherDownloads = "Voucher Downloads",
+    adDownloads = "Ad Downloads",
+    generatedDownloads = "Generated Downloads",
+}
