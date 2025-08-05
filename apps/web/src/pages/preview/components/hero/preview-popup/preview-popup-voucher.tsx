d63352534f4aff3332e203ae7f819d365169ab56
iDarkQ <matikuki3@gmail.com>
2025-08-05 16:25:07 +0100

    refactor(preview-popup): utilize single responsibility

---
+++ apps/web/src/pages/preview/components/hero/preview-popup/preview-popup-voucher.tsx
@@ -0,0 +1,31 @@
+import { BedrockText } from "~/components/bedrock/bedrock-text";
+import { Button } from "~/components/bedrock/button";
+import { Input } from "~/components/bedrock/input";
+import { styles } from ".";
+
+interface PreviewPopupVoucherProps {
+  voucherCode: string;
+  setVoucherCode: (value: React.SetStateAction<string>) => void;
+  handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => Promise<void>;
+  useVoucher: () => Promise<void>;
+}
+
+export const PreviewPopupVoucher = ({
+  voucherCode,
+  setVoucherCode,
+  handleKeyDown,
+  useVoucher,
+}: PreviewPopupVoucherProps) => (
+  <div className={styles.voucher}>
+    <Input
+      placeholder="Voucher Code"
+      className={styles.input}
+      value={voucherCode}
+      onKeyDown={handleKeyDown}
+      onChange={(e) => setVoucherCode(e.target.value)}
+    />
+    <Button type="dark" onClick={useVoucher} center>
+      <BedrockText color="white" type="p" text="Apply" />
+    </Button>
+  </div>
+);
