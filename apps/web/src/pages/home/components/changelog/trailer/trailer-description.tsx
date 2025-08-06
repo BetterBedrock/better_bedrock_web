66d70ca3224fbb9494af1c929cd75145f51a745d
iDarkQ <matikuki3@gmail.com>
2025-08-06 20:41:06 +0100

    refactor(changelog/trailer): replace 7.2 version with 8.0

--- apps/web/src/pages/home/components/changelog/trailer/trailer-description.tsx
+++ apps/web/src/pages/home/components/changelog/trailer/trailer-description.tsx
@@ -1,5 +1,5 @@
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 
 export const TrailerDescription = () => (
-  <BedrockText type="p" color="white" text="Watch Better Bedrock v7.2 Changelog Showcase" />
+  <BedrockText type="p" color="white" text="Watch Better Bedrock v8.0 Changelog Showcase" />
 );
