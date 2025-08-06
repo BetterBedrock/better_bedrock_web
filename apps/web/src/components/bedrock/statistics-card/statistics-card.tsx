2a2467fa5129ba22f51a5aae9713c239afa8b0b3
iDarkQ <matikuki3@gmail.com>
2025-08-06 20:40:26 +0100

    refactor(statistics-card): replace single quote with double quote & add highlight

--- apps/web/src/components/bedrock/statistics-card/statistics-card.tsx
+++ apps/web/src/components/bedrock/statistics-card/statistics-card.tsx
@@ -7,15 +7,15 @@ import { Card } from "~/components/bedrock/card";
 export const StatisticsCard = ({
   name,
   data = [],
-  range = '7d',
+  range = "7d",
   showGraph = true,
-  suffix = '',
+  suffix = "",
   ...props
 }: StatisticsCardProps) => {
   let total = 0;
   let fdata: FData | null = null;
 
-  if (typeof data === 'number') {
+  if (typeof data === "number") {
     total = data;
   } else {
     fdata = transformToFData(data, range);
@@ -25,13 +25,21 @@ export const StatisticsCard = ({
   return (
     <Card {...props}>
       <div className={styles.statistics}>
-        <BedrockText extraClassName={styles.name} type="h3" text={name} textAlign="left" color="grey" />
         <BedrockText
+          extraClassName={styles.name}
+          type="h3"
+          text={name}
+          textAlign="left"
+          color="grey"
+        />
+        <BedrockText
+          key={total}
           type="h2"
           text={`Total ${total.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 4 })}${suffix}`}
           textAlign="left"
           color="white"
           font="Minecraft"
+          extraClassName={styles.highlight}
         />
         {showGraph && fdata && <BarChart direction="horizontal" data={fdata} />}
       </div>
