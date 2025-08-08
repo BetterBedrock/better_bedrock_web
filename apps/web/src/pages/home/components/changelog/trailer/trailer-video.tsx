162fdd7ef08a39ace4e5cd546ad170fe693d57d9
iDarkQ <matikuki3@gmail.com>
2025-08-08 11:08:12 +0100

    refactor(trailer-video.tsx): change video url

--- apps/web/src/pages/home/components/changelog/trailer/trailer-video.tsx
+++ apps/web/src/pages/home/components/changelog/trailer/trailer-video.tsx
@@ -6,7 +6,7 @@ export const TrailerVideo = () => (
       width="100%"
       height="100%"
       // todo: update 
-      src="https://www.youtube.com/embed/Oz2cnztzSMw"
+      src="https://youtube.com/embed/DEQu3Q19ZMM"
       title="Better Bedrock Changelog v8.0 #1 | Client release on windows! | New Texture Pack version | Showcase"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
