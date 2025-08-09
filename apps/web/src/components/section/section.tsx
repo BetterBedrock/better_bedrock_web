e8ee2a4b548f3fe9470e06c9c558d70022cbeb31
iDarkQ <matikuki3@gmail.com>
2025-08-09 15:11:12 +0100

    fix(web)!: background images not working well when background-attachment: fixed

--- apps/web/src/components/section/section.tsx
+++ apps/web/src/components/section/section.tsx
@@ -10,8 +10,20 @@ interface SectionProps extends HTMLAttributes<HTMLDivElement> {
   extraClassName?: string;
 }
 
-export const Section = ({ children, fixed, center, className, extraClassName, ...props }: SectionProps) => (
-  <div className={clsx(styles.wrapper, fixed && styles.fixed, className && className)} {...props}>
-    <section className={clsx(styles.section, center && styles.center, extraClassName && extraClassName)}>{children}</section>
+export const Section = ({
+  children,
+  fixed,
+  center,
+  className,
+  extraClassName,
+  ...props
+}: SectionProps) => (
+  <div className={clsx(styles.wrapper, !fixed && className && className)} {...props}>
+    {fixed && <div className={clsx(fixed && styles.fixed, className && className)} />}
+    <section
+      className={clsx(styles.section, center && styles.center, extraClassName && extraClassName)}
+    >
+      {children}
+    </section>
   </div>
 );
