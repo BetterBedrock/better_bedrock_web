84d89e7ac25c3d697b3f6e0fe395dbdff39b5058
iDarkQ <matikuki3@gmail.com>
2025-08-12 06:38:30 +0100

    refactor(auth.conotroller.ts): add api responses

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import {
     ApiBearerAuth,
+    ApiCreatedResponse,
     ApiInternalServerErrorResponse,
     ApiOkResponse,
     ApiTags,
@@ -11,14 +12,15 @@ import { AuthService } from "~/auth/auth.service";
 import { AuthorizeDto } from "~/auth/dto/authorize.dto";
 import { JwtTokenDto } from "~/auth/dto/jwt-token.dto";
 import { UserAuthGuard } from "~/auth/user-auth.guard";
+import { UserDto } from "~/user/dto/user.dto";
 
 @ApiTags("auth")
 @Controller("auth")
 export class AuthController {
-    constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService) {}
 
     @Post()
-    @ApiOkResponse({
+    @ApiCreatedResponse({
         description: "Successfully authorized connection with account ",
         type: JwtTokenDto,
     })
@@ -27,9 +29,10 @@ export class AuthController {
     }
 
     @Get()
+    @ApiOkResponse({ description: "Successfully authenticated", type: UserDto })
     @UseGuards(UserAuthGuard)
     @ApiBearerAuth()
-    async authenticate(@Req() req) {
+    async authenticate(@Req() req): Promise<UserDto> {
         return req.user;
     }
 
