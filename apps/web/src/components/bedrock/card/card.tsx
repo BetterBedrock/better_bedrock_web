c73ee8d76515bd15a035688918c2a3f0e34812f0
iDarkQ <matikuki3@gmail.com>
2025-08-15 00:55:34 +0100

    feat(card): create sub cards

--- apps/web/src/components/bedrock/card/card.tsx
+++ apps/web/src/components/bedrock/card/card.tsx
@@ -1,16 +1,26 @@
+import React from "react";
 import clsx from "clsx";
 import { styles } from ".";
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
   children?: React.ReactNode;
+  sub?: boolean;
 }
 
-export const Card = ({ className, children, ...rest }: CardProps) => (
-  <div className={clsx(styles.container, className && className)} {...rest}>
-    {children}
-  </div>
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, children, sub, ...rest }, ref) => (
+    <div
+      ref={ref}
+      className={clsx(styles.container, sub && styles.sub, className)}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
 );
+
+Card.displayName = "Card";
 
 export const CardDivider = () => (
   <div>
