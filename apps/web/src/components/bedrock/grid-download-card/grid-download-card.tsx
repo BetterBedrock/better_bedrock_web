4a62fe38120582eb3ac5b316d998dff2d00829b3
iDarkQ <matikuki3@gmail.com>
2025-08-15 00:55:22 +0100

    refactor(grid-card): change layout for the download grid cards

--- apps/web/src/components/bedrock/grid-download-card/grid-download-card.tsx
+++ apps/web/src/components/bedrock/grid-download-card/grid-download-card.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import { BedrockText } from "../bedrock-text/bedrock-text";
 import styles from "./grid-download-card.module.scss";
 import clsx from "clsx";
-
-// The Button and ButtonSeparator components will now be passed in via the `actions` prop.
+import Steve from "~/assets/images/avatars/Steve.png";
+import { Tag } from "~/components/bedrock/tag";
+import { Rating } from "~/components/rating";
 
 interface GridDownloadCardProps {
   title?: string;
   downloadSize?: string;
-  thumbnail?: React.ReactNode;
-  /** Allows passing custom components, not just strings */
+  thumbnail?: string;
   description?: React.ReactNode;
-  /** Accepts any React node to render in the footer, like buttons */
   actions?: React.ReactNode;
-  /** Allows for adding custom CSS classes for further styling */
   className?: string;
   useTopDivider?: boolean;
-  tags?: string[]; // Changed to string[] to allow for any tags
+  tags?: string[];
 }
 
 export const GridDownloadCard: React.FC<GridDownloadCardProps> = ({
@@ -26,78 +24,80 @@ export const GridDownloadCard: React.FC<GridDownloadCardProps> = ({
   thumbnail,
   actions,
   className,
-  useTopDivider = false,
-  tags
+  // useTopDivider = false,
+  tags,
 }) => {
   return (
     <div className={clsx(styles.container, className)}>
+      <div className={styles.header}>
+        <img src={Steve} className={styles.avatar} />
+
+        {/* Renders the description, which can now be any React node */}
+        <div>
+          {description}
+          <Rating simple rating={2.5} />
+        </div>
+      </div>
+
       {thumbnail && (
         <div className={styles.thumbnailWrapper}>
-          {thumbnail}
-          <div className={styles.imgWrapper}>
+          <img src={thumbnail} alt={`Thumbnail for ${title}`} className={styles.thumbnail} />
+          <div className={styles.details}>
+            <div className={styles.cardTitle}>
+              <BedrockText
+                text={title ?? ""}
+                type="h2"
+                font="MinecraftTen"
+                textAlign="left"
+                color="white"
+                style={{ padding: "0 0.5rem 0 0" }}
+              />
+              <BedrockText
+                text={downloadSize ?? ""}
+                type={"h3"}
+                font="MinecraftTen"
+                color="white"
+                textAlign="left"
+                extraClassName={styles.downloadSize}
+              />
+            </div>
+          </div>
+          <div className={styles.tags}>
+            {tags?.map((tag) => (
+              <Tag name={tag} />
+            ))}
+          </div>
+
+          {/* <div className={styles.imgWrapper}>
             {tags && (
               <div className={styles.tagsWrapper}>
                 <div className={styles.tagsContainer}>
-                  {tags.map(tag => {
+                  {tags.map((tag) => {
                     return (
-                      < BedrockText
+                      <BedrockText
                         text={tag}
                         type={"p"}
                         textAlign="left"
                         color={"black"}
                         style={{
-                          backgroundColor: "rgb(140, 179, 255)"
+                          backgroundColor: "rgb(140, 179, 255)",
                         }}
                       />
-                    )
+                    );
                   })}
                 </div>
               </div>
             )}
-          </div>
+          </div> */}
         </div>
       )}
-
-      {useTopDivider && <GridDownloadCardDivider />}
-      <div className={styles.texts}>
-        <div className={styles.cardTitle}>
-          <BedrockText
-            text={title ?? ""}
-            type="h2"
-            font="MinecraftTen"
-            textAlign="left"
-            color="white"
-            style={{ padding: "0 0.5rem 0 0" }}
-          />
-          <BedrockText
-            text={downloadSize ?? ""}
-            type={"h3"}
-            font="MinecraftTen"
-            color="white"
-            textAlign="left"
-            extraClassName={styles.downloadSize}
-          />
-        </div>
-        {/* Renders the description, which can now be any React node */}
-        {description}
-      </div>
 
       {/* If actions are provided, a divider is shown and they are rendered */}
       {actions && (
         <>
-          <GridDownloadCardDivider />
           <div className={styles.actions}>{actions}</div>
         </>
       )}
     </div>
-  );
-};
-
-export const GridDownloadCardDivider = () => {
-  return (
-    <>
-      <div className={styles.top}></div>
-      <div className={styles.bottom}></div>
-    </>
   );
 };
