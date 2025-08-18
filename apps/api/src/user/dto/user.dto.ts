752eef76f8aad83ea8c9671339d8b79cc931bf1d
iDarkQ <matikuki3@gmail.com>
2025-08-18 21:09:11 +0100

    refactor(user.dto.ts): add decorators for property types

--- apps/api/src/user/dto/user.dto.ts
+++ apps/api/src/user/dto/user.dto.ts
@@ -1,9 +1,37 @@
+import { Type } from "class-transformer";
+import { IsBoolean, IsDate, IsOptional, IsString } from "class-validator";
+
 export class UserDto {
+    @IsString()
     id: string;
+
+    @IsString()
     googleId: string;
+
+    @IsString()
     email: string;
+
+    @IsString()
     name: string;
+
+    @IsString()
     bio: string;
+
+    @IsBoolean()
     banned: boolean;
+
+    @IsDate()
+    @Type(() => Date)
     createdAt: Date;
+
+    @IsBoolean()
+    customLinkvertise: boolean;
+
+    @IsString()
+    @IsOptional()
+    linkvertiseId: string | null;
+
+    @IsString()
+    @IsOptional()
+    linkvertiseSecret: string | null;
 }
