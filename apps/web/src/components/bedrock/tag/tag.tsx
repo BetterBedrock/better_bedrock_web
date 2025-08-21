f2338a57cd84fb15c6748e970a39ead9d38f2b8f
iDarkQ <matikuki3@gmail.com>
2025-08-21 21:27:17 +0100

    refactor(tag): add option to choose border type

--- apps/web/src/components/bedrock/tag/tag.tsx
+++ apps/web/src/components/bedrock/tag/tag.tsx
@@ -1,10 +1,34 @@
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 import { styles } from ".";
+import clsx from "clsx";
+
+type Border = "left" | "right" | "bottom" | "top" | "all";
 
 interface TagProps {
   name: string;
+  border: Border | Border[];
+  onDelete?: () => void;
+  deletable?: boolean;
+  // border:
 }
 
-export const Tag = ({ name }: TagProps) => (
-  <BedrockText text={name} type="p" textAlign="center" color="black" extraClassName={styles.tag} />
+export const Tag = ({ name, border, onDelete, deletable }: TagProps) => (
+  <div
+    className={clsx(
+      styles.tag,
+      Array.isArray(border) ? border.map((b) => styles[b]) : styles[border],
+    )}
+  >
+    <BedrockText text={name} type="p" textAlign="center" color="black" />
+    {deletable && (
+      <BedrockText
+        text="x"
+        type="p"
+        textAlign="center"
+        color="black"
+        onClick={onDelete}
+        extraClassName={styles.delete}
+      />
+    )}
+  </div>
 );
