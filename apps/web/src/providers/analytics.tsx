25aa377b481050191c2f24a8cf91bb72d0955382
iDarkQ <matikuki3@gmail.com>
2025-08-21 21:27:43 +0100

    feat(analytics.tsx): add option to fetch user analytics

--- apps/web/src/providers/analytics.tsx
+++ apps/web/src/providers/analytics.tsx
@@ -8,6 +8,7 @@ import { baseUrl } from "~/utils/url";
 interface AnalyticsContextProps {
   analytics: AnalyticsDto[];
   fetchAnalytics: () => Promise<void>;
+  fetchUserAnalytics: () => Promise<AnalyticsDto[] | undefined>;
 }
 
 interface AnalyticsProvider {
@@ -17,23 +18,38 @@ interface AnalyticsProvider {
 const AnalyticsContext = createContext<AnalyticsContextProps | undefined>(undefined);
 
 export const AnalyticsProvider = ({ children }: AnalyticsProvider) => {
-  const [cookie] = useCookies(["adminSecret"]);
+  const [cookie] = useCookies(["adminSecret", "secret"]);
   const [analytics, setAnalytics] = useState<AnalyticsDto[]>([]);
   const { throwError } = useNotification();
   const { authenticated } = useAuth();
 
-  const config = new Configuration({
+  const adminConfig = new Configuration({
     basePath: baseUrl,
     accessToken: cookie.adminSecret,
   });
 
+  const config = new Configuration({
+    basePath: baseUrl,
+    accessToken: cookie.secret,
+  });
+
+  const adminAnalyticsApi = new AnalyticsApi(adminConfig);
   const analyticsApi = new AnalyticsApi(config);
 
   const fetchAnalytics = async () => {
     try {
-      const { data } = await analyticsApi.analyticsControllerAnalytics();
+      const { data } = await adminAnalyticsApi.analyticsControllerAnalytics();
 
       setAnalytics(data);
+    } catch (err) {
+      throwError(err, "Failed to fetch analytics");
+    }
+  };
+
+  const fetchUserAnalytics = async (): Promise<AnalyticsDto[] | undefined> => {
+    try {
+      const { data } = await analyticsApi.analyticsControllerUser();
+      return data;
     } catch (err) {
       throwError(err, "Failed to fetch analytics");
     }
@@ -43,11 +59,11 @@ export const AnalyticsProvider = ({ children }: AnalyticsProvider) => {
     if (!authenticated) return;
     fetchAnalytics();
 
-    setInterval(fetchAnalytics, 2000)
+    setInterval(fetchAnalytics, 2000);
   }, [authenticated]);
 
   return (
-    <AnalyticsContext.Provider value={{ analytics, fetchAnalytics }}>
+    <AnalyticsContext.Provider value={{ analytics, fetchUserAnalytics, fetchAnalytics }}>
       {children}
     </AnalyticsContext.Provider>
   );
