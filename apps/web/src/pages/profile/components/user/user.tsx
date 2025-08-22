7298dbeda5f39a79073499063b2bbe7a54f545bb
iDarkQ <matikuki3@gmail.com>
2025-08-22 21:32:07 +0100

    refactor(user): change user page

--- apps/web/src/pages/profile/components/user/user.tsx
+++ apps/web/src/pages/profile/components/user/user.tsx
@@ -3,25 +3,84 @@ import { styles } from ".";
 import Steve from "~/assets/images/avatars/Steve.png";
 // import { Label } from "~/components/bedrock/label";
 import { Card } from "~/components/bedrock/card";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { Rating } from "~/components/rating";
 import { Tabs } from "~/pages/profile/components/user/tabs";
+import { useUser } from "~/providers/user";
+import { useEffect, useState } from "react";
+import { SimpleUserDto } from "~/lib/api";
+import { Routes } from "~/utils/routes";
+import { CircularProgressIndicator } from "~/components/bedrock/circular-progress-indicator";
+import { SimpleButton } from "~/components/bedrock/simple-button";
+import SettingsGlyph from "~/assets/images/settings_glyph.png";
+import { UserSettingsForm } from "~/pages/profile/components/user/user-settings-form";
+import { useAuth } from "~/providers/auth";
 
-export const User = () => (
-  <div className={styles.wrapper}>
-    <Card>
-      <div className={styles.profile}>
-        <img src={Steve} className={styles.image} />
-        <div className={styles.profileInfo}>
-          <BedrockText type="h1" text="iDarkQ" color="white" textAlign="left" font="Minecraft" />
-          <BedrockText type="p" text="Lorem ipsum dolor sit amet" color="white" textAlign="left" />
-          <Rating rating={2.56} />
+export const User = () => {
+  const { id } = useParams();
+  const { findUserByName, updateProfile } = useUser();
+  const { user } = useAuth();
+  const [selectedUser, setSelectedUser] = useState<SimpleUserDto | undefined>();
+  const navigate = useNavigate();
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
+  useEffect(() => {
+    if (!id) {
+      navigate(Routes.HOME);
+      return;
+    }
+    findUserByName(id).then((data) => {
+      setSelectedUser(data);
+    });
+  }, []);
+
+  if (!selectedUser) {
+    return <CircularProgressIndicator size="medium" />;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {settingsOpen && user && (
+        <UserSettingsForm
+          user={user}
+          onClose={() => setSettingsOpen(false)}
+          onSave={async (data) => {
+            console.log("Begin updating profile");
+            const newUser = await updateProfile(data);
+            console.log("Finish updating profile");
+            if (!newUser) return;
+            console.log("Navigate");
+            setSelectedUser(newUser);
+            navigate(Routes.PROFILE + "/" + newUser.name + "/projects");
+            console.log(data);
+          }}
+        />
+      )}
+      <Card>
+        <div className={styles.profile}>
+          <img src={Steve} className={styles.image} />
+          <div className={styles.info}>
+            <div className={styles.name}>
+              <BedrockText
+                type="h1"
+                text={selectedUser.name}
+                color="white"
+                textAlign="left"
+                font="Minecraft"
+              />
+              <SimpleButton transparent onClick={() => setSettingsOpen(true)}>
+                <img src={SettingsGlyph} className={styles.icon} />
+              </SimpleButton>
+            </div>
+            <Rating rating={2.56} />
+            <BedrockText type="p" text={selectedUser.bio} color="white" textAlign="left" />
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
 
-    <Tabs />
+      <Tabs />
 
-    <Outlet />
-  </div>
-);
+      <Outlet context={{ selectedUser }} />
+    </div>
+  );
+};
