301652e718e055b42f448df72b56d7e106c8078b
iDarkQ <matikuki3@gmail.com>
2025-08-22 21:31:38 +0100

    refactor(projects): simplify grid cards & pre fetch projects

--- apps/web/src/pages/profile/components/user/projects/projects.tsx
+++ apps/web/src/pages/profile/components/user/projects/projects.tsx
@@ -1,49 +1,34 @@
-import { useNavigate } from "react-router-dom";
-import { BedrockText } from "~/components/bedrock/bedrock-text";
-import { Button } from "~/components/bedrock/button";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { GridDownloadCard } from "~/components/bedrock/grid-download-card/grid-download-card";
-import { ButtonGroup } from "~/components/button-group/button-group";
-import { Link } from "~/components/link";
+
 import { styles } from ".";
 import { Routes } from "~/utils/routes";
-import { baseUrl } from "~/utils/url";
+import { useState, useEffect } from "react";
+import { SimpleUserDto, SimpleProjectDto } from "~/lib/api";
+import { useProject } from "~/providers/project";
 
 export const Projects = () => {
   const navigate = useNavigate();
+  const { selectedUser } = useOutletContext<{ selectedUser: SimpleUserDto }>();
+  const { fetchUserProjects } = useProject();
+  const [projects, setProjects] = useState<SimpleProjectDto[]>([]);
+
+  useEffect(() => {
+    if (!selectedUser) {
+      navigate(Routes.HOME);
+      return;
+    }
+    fetchUserProjects(selectedUser.id).then((data) =>
+      setProjects(data?.filter((d) => d.draft === false) ?? []),
+    );
+  }, []);
+
   return (
     <div className={styles.list}>
       <div className={styles.projects}>
-        <GridDownloadCard
-          // key={item.downloadId}
-          title={"Test"}
-          tags={["deprected"]}
-          description={<BedrockText text={`@iDarkQ`} type={"p"} textAlign="left" color="white" />}
-          thumbnail={`${baseUrl}/static/uploads/public/images/themes/v8/blue_ui/1.png`}
-          actions={
-            <ButtonGroup>
-              <Button
-                // onClick={() => setDownloadItem(item)}
-                width="100%"
-                height="auto"
-                type="green"
-                center
-              >
-                <BedrockText color="white" text="Download" type="p" />
-              </Button>
-              <Link link={Routes.PREVIEW + "/" + "blue_ui"}>
-                <Button
-                  width="100%"
-                  height="auto"
-                  type="white"
-                  center
-                  onClick={() => navigate(`${Routes.PREVIEW}/${"blue_ui.mcpack"}`)}
-                >
-                  <BedrockText color="black" text="Preview" type="p" />
-                </Button>
-              </Link>
-            </ButtonGroup>
-          }
-        />
+        {projects.map((project, index) => (
+          <GridDownloadCard key={index} project={project} mode="view" />
+        ))}
       </div>
     </div>
   );
