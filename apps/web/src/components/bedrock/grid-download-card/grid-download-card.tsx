d29bffdf58104851a0b605a86455b4c711278ae2
iDarkQ <matikuki3@gmail.com>
2025-08-22 21:30:49 +0100

    refactor(grid-download-card): simplify display, and allow for creating grid download with just project object

--- apps/web/src/components/bedrock/grid-download-card/grid-download-card.tsx
+++ apps/web/src/components/bedrock/grid-download-card/grid-download-card.tsx
@@ -5,8 +5,16 @@ import clsx from "clsx";
 import Steve from "~/assets/images/avatars/Steve.png";
 import { Tag } from "~/components/bedrock/tag";
 import { Rating } from "~/components/rating";
+import { SimpleProjectDto } from "~/lib/api";
+import { baseUrl } from "~/utils/url";
+import { Link } from "~/components/link";
+import { Routes } from "~/utils/routes";
+import { PreviewMode } from "~/pages/preview";
 
 interface GridDownloadCardProps {
+  project: SimpleProjectDto;
+  mode?: PreviewMode;
+
   title?: string;
   downloadSize?: string;
   thumbnail?: string;
@@ -17,87 +25,71 @@ interface GridDownloadCardProps {
   tags?: string[];
 }
 
-export const GridDownloadCard: React.FC<GridDownloadCardProps> = ({
-  title,
-  downloadSize,
-  description,
-  thumbnail,
-  actions,
+export const GridDownloadCard = ({
   className,
-  // useTopDivider = false,
-  tags,
-}) => {
+
+  project,
+  mode,
+}: GridDownloadCardProps) => {
+  const link = () => {
+    if (mode === "edit") return Routes.EDITOR + "/" + project.id;
+    if (mode === "review") return Routes.REVIEW + "/" + project.id;
+    return Routes.PREVIEW + "/" + project.id;
+  };
+
   return (
     <div className={clsx(styles.container, className)}>
-      <div className={styles.header}>
-        <img src={Steve} className={styles.avatar} />
+      <Link link={Routes.PROFILE + "/" + project.user.name} className={styles.link}>
+        <div className={styles.header}>
+          <img src={Steve} className={styles.avatar} />
 
-        {/* Renders the description, which can now be any React node */}
-        <div>
-          {description}
-          <Rating simple rating={2.5} />
+          {/* Renders the description, which can now be any React node */}
+          <div>
+            <BedrockText text={`@${project.user.name}`} type={"p"} textAlign="left" color="white" />
+
+            <Rating simple rating={2.5} />
+          </div>
         </div>
-      </div>
+      </Link>
 
-      {thumbnail && (
-        <div className={styles.thumbnailWrapper}>
-          <img src={thumbnail} alt={`Thumbnail for ${title}`} className={styles.thumbnail} />
-          <div className={styles.details}>
-            <div className={styles.cardTitle}>
+      <Link link={link()} className={styles.link}>
+        <div className={styles.background}>
+          {!project.thumbnail || project.thumbnail === "" ? (
+            <div className={styles.empty}>
               <BedrockText
-                text={title ?? ""}
+                text={"404 No Thumbnail"}
+                type="h1"
+                font="MinecraftTen"
+                textAlign="left"
+                color="white"
+              />
+            </div>
+          ) : (
+            <img
+              src={baseUrl + "/" + project.thumbnail}
+              alt={`Thumbnail for ${project.title}`}
+              className={styles.thumbnail}
+            />
+          )}
+
+          <div className={styles.details}>
+            <div className={styles.title}>
+              <BedrockText
+                text={project.title}
                 type="h2"
                 font="MinecraftTen"
                 textAlign="left"
                 color="white"
-                style={{ padding: "0 0.5rem 0 0" }}
-              />
-              <BedrockText
-                text={downloadSize ?? ""}
-                type={"h3"}
-                font="MinecraftTen"
-                color="white"
-                textAlign="left"
-                extraClassName={styles.downloadSize}
               />
             </div>
           </div>
           <div className={styles.tags}>
-            {tags?.map((tag) => (
-              <Tag name={tag} />
+            {project.tags.map((tag) => (
+              <Tag border={["left", "bottom"]} name={tag} />
             ))}
           </div>
-
-          {/* <div className={styles.imgWrapper}>
-            {tags && (
-              <div className={styles.tagsWrapper}>
-                <div className={styles.tagsContainer}>
-                  {tags.map((tag) => {
-                    return (
-                      <BedrockText
-                        text={tag}
-                        type={"p"}
-                        textAlign="left"
-                        color={"black"}
-                        style={{
-                          backgroundColor: "rgb(140, 179, 255)",
-                        }}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-            )}
-          </div> */}
         </div>
-      )}
-
-      {/* If actions are provided, a divider is shown and they are rendered */}
-      {actions && (
-        <>
-          <div className={styles.actions}>{actions}</div>
-        </>
-      )}
+      </Link>
     </div>
   );
 };
