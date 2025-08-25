0d8c01b83ffb1a477eda289fe98d96f43ef055a9
iDarkQ <matikuki3@gmail.com>
2025-08-25 09:57:11 +0100

    feat(stats): create stats page for user

---
+++ apps/web/src/pages/profile/components/user/stats/index.ts
@@ -0,0 +1,2 @@
+export * from "./stats";
+export { default as styles } from "./stats.module.scss";
---
+++ apps/web/src/pages/profile/components/user/stats/stats.tsx
@@ -0,0 +1,42 @@
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { styles } from ".";
+import { Routes } from "~/utils/routes";
+import { useState, useEffect } from "react";
+import { SimpleUserDto, AnalyticsDto } from "~/lib/api";
+import { useAnalytics } from "~/providers/analytics";
+import { StatisticsCard } from "~/components/bedrock/statistics-card";
+
+export const Stats = () => {
+  const navigate = useNavigate();
+  const { selectedUser } = useOutletContext<{ selectedUser: SimpleUserDto }>();
+  const { fetchUserAnalytics } = useAnalytics();
+
+  const [analytics, setAnalytics] = useState<AnalyticsDto[]>([]);
+
+  useEffect(() => {
+    if (!selectedUser) {
+      navigate(Routes.HOME);
+      return;
+    }
+    fetchUserAnalytics().then((data) => setAnalytics(data ?? []));
+  }, []);
+
+  console.log({ analytics });
+
+  const categories = analytics.filter((value) => value.type === "file").map((value) => value.name);
+
+  const data = categories.reduce((acc: { [key: string]: typeof analytics }, category) => {
+    acc[category] = analytics.filter((a) => a.name === category);
+    return acc;
+  }, {});
+
+  return (
+    <div className={styles.list}>
+      <div className={styles.projects}>
+        {Object.keys(data).map((name) => (
+          <StatisticsCard name={name} data={data[name]} className={styles.card} />
+        ))}
+      </div>
+    </div>
+  );
+};
