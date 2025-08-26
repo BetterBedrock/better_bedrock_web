fec5cca332b24d91015e08a0d11e378b13b7bfaa
iDarkQ <matikuki3@gmail.com>
2025-08-26 10:08:31 +0100

    feat(user.tsx): make analytics & stats work

--- apps/web/src/pages/profile/components/user/user.tsx
+++ apps/web/src/pages/profile/components/user/user.tsx
@@ -8,29 +8,35 @@ import { Rating } from "~/components/rating";
 import { Tabs } from "~/pages/profile/components/user/tabs";
 import { useUser } from "~/providers/user";
 import { useEffect, useState } from "react";
-import { SimpleUserDto } from "~/lib/api";
+import { SimpleUserDto, UserRatingDto } from "~/lib/api";
 import { Routes } from "~/utils/routes";
 import { CircularProgressIndicator } from "~/components/bedrock/circular-progress-indicator";
 import { SimpleButton } from "~/components/bedrock/simple-button";
 import SettingsGlyph from "~/assets/images/settings_glyph.png";
 import { UserSettingsForm } from "~/pages/profile/components/user/user-settings-form";
 import { useAuth } from "~/providers/auth";
+import { AnalyticsProvider } from "~/providers/analytics";
 
 export const User = () => {
   const { id } = useParams();
-  const { findUserByName, updateProfile } = useUser();
+  const { findUserByName, updateProfile, getProfileRating } = useUser();
   const { user } = useAuth();
   const [selectedUser, setSelectedUser] = useState<SimpleUserDto | undefined>();
   const navigate = useNavigate();
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const [rating, setRating] = useState<UserRatingDto | undefined>(undefined);
 
   useEffect(() => {
     if (!id) {
       navigate(Routes.HOME);
       return;
     }
-    findUserByName(id).then((data) => {
+    findUserByName(id).then(async (data) => {
       setSelectedUser(data);
+      if (data) {
+        const userRating = await getProfileRating(data.id);
+        setRating(userRating);
+      }
     });
   }, []);
 
@@ -39,48 +45,50 @@ export const User = () => {
   }
 
   return (
-    <div className={styles.wrapper}>
-      {settingsOpen && user && (
-        <UserSettingsForm
-          user={user}
-          onClose={() => setSettingsOpen(false)}
-          onSave={async (data) => {
-            console.log("Begin updating profile");
-            const newUser = await updateProfile(data);
-            console.log("Finish updating profile");
-            if (!newUser) return;
-            console.log("Navigate");
-            setSelectedUser(newUser);
-            navigate(Routes.PROFILE + "/" + newUser.name + "/projects");
-            console.log(data);
-          }}
-        />
-      )}
-      <Card>
-        <div className={styles.profile}>
-          <img src={Steve} className={styles.image} />
-          <div className={styles.info}>
-            <div className={styles.name}>
-              <BedrockText
-                type="h1"
-                text={selectedUser.name}
-                color="white"
-                textAlign="left"
-                font="Minecraft"
-              />
-              <SimpleButton transparent onClick={() => setSettingsOpen(true)}>
-                <img src={SettingsGlyph} className={styles.icon} />
-              </SimpleButton>
+    <AnalyticsProvider>
+      <div className={styles.wrapper}>
+        {settingsOpen && user && (
+          <UserSettingsForm
+            user={user}
+            onClose={() => setSettingsOpen(false)}
+            onSave={async (data) => {
+              console.log("Begin updating profile");
+              const newUser = await updateProfile(data);
+              console.log("Finish updating profile");
+              if (!newUser) return;
+              console.log("Navigate");
+              setSelectedUser(newUser);
+              navigate(Routes.PROFILE + "/" + newUser.name + "/projects");
+              console.log(data);
+            }}
+          />
+        )}
+        <Card>
+          <div className={styles.profile}>
+            <img src={Steve} className={styles.image} />
+            <div className={styles.info}>
+              <div className={styles.name}>
+                <BedrockText
+                  type="h1"
+                  text={selectedUser.name}
+                  color="white"
+                  textAlign="left"
+                  font="Minecraft"
+                />
+                <SimpleButton transparent onClick={() => setSettingsOpen(true)}>
+                  <img src={SettingsGlyph} className={styles.icon} />
+                </SimpleButton>
+              </div>
+              {rating && <Rating rating={rating.average} suffix={`(${rating.count} Reviews)`}/>}
+              <BedrockText type="p" text={selectedUser.bio} color="white" textAlign="left" />
             </div>
-            <Rating rating={2.56} />
-            <BedrockText type="p" text={selectedUser.bio} color="white" textAlign="left" />
           </div>
-        </div>
-      </Card>
+        </Card>
 
-      <Tabs />
+        <Tabs />
 
-      <Outlet context={{ selectedUser }} />
-    </div>
+        <Outlet context={{ selectedUser }} />
+      </div>
+    </AnalyticsProvider>
   );
 };
