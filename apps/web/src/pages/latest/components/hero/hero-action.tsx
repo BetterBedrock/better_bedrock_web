2d46e0a47857dde95ceac82001323d5ba6f759e1
iDarkQ <matikuki3@gmail.com>
2025-08-30 09:03:48 +0100

    fix(hero-action.tsx): hide show popup for now, until everything gets connected

--- apps/web/src/pages/latest/components/hero/hero-action.tsx
+++ apps/web/src/pages/latest/components/hero/hero-action.tsx
@@ -1,31 +1,28 @@
 import { useState } from "react";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 import { Button } from "~/components/bedrock/button";
-// import { ButtonGroup } from "~/components/button-group/button-group";
-import { DownloadsItemDto } from "~/lib/api";
-import { useContent } from "~/providers/content";
 import { styles } from ".";
 // import { useNavigate } from "react-router-dom";
 // import { Routes } from "~/utils/routes";
-import { PreviewPopup } from "~/pages/preview/components/hero/preview-popup";
+import { MAIN_LIST } from "~/assets/content/better-bedrock";
+import { DownloadsItemDto } from "~/assets/content/dto/downloads-item.dto";
 
 export const HeroAction = () => {
   // const navigate = useNavigate();
-  const { downloads } = useContent();
-  const [downloadItem, setDownloadItem] = useState<DownloadsItemDto | null>(null);
+  const [_, setDownloadItem] = useState<DownloadsItemDto | null>(null);
 
-  const featuredCategory = downloads?.categories.flatMap((category) => category.lists).flatMap((list) => list.items);
-  const featuredItem = featuredCategory?.find((item) => item.downloadId === downloads?.featured);
+  const featuredCategory = MAIN_LIST;
+  const featuredItem = featuredCategory.lists[0];
 
-  if (!downloads || !featuredItem) {
+  if (!featuredItem) {
     return <></>;
   }
 
   return (
     <>
-      {downloadItem && (
+      {/* {downloadItem && (
         <PreviewPopup onClose={() => setDownloadItem(null)} downloadItem={downloadItem} />
-      )}
+      )} */}
       <div className={styles.actions}>
         <Button width="100%" type="green" onClick={() => setDownloadItem(featuredItem)} center>
           <BedrockText text="Download Now!" type="p" color="white" />
