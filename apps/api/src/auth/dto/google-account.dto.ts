6f65288778d8dfe9cdffba96ba66fb4724f310a2
iDarkQ <matikuki3@gmail.com>
2025-09-01 19:05:12 +0100

    refactor(google-account.dto.ts): pick types from userdto, and missing ones add manually

--- apps/api/src/auth/dto/google-account.dto.ts
+++ apps/api/src/auth/dto/google-account.dto.ts
@@ -1,6 +1,16 @@
-export class GoogleAccountDto {
+import { PickType } from "@nestjs/swagger";
+import { UserDto } from "~/user/dto/user.dto";
+
+export class GoogleAccountDto extends PickType(UserDto, ["email", "name"]) {
+    /**
+     * Google account identifier
+     * @example "123abc456efg"
+     */
     sub: string;
-    email: string;
-    name: string;
+
+    /**
+     * Link to google account profile picture
+     * @example "https://..."
+     */
     picture: string;
 }
