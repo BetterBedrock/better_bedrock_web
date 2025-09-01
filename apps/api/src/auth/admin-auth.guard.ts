84ef4ebdf69ca191e398e0fa0fd5ed80eda44d3d
iDarkQ <matikuki3@gmail.com>
2025-09-01 19:09:57 +0100

    refactor(admin-auth.guard.ts): replace admin authentication by env secret with checking if user is admin

--- apps/api/src/auth/admin-auth.guard.ts
+++ apps/api/src/auth/admin-auth.guard.ts
@@ -1,26 +1,46 @@
 import {
     CanActivate,
     ExecutionContext,
+    ForbiddenException,
     Injectable,
-    InternalServerErrorException,
     UnauthorizedException,
 } from "@nestjs/common";
 
-import { Request } from "express";
+import { JwtService } from "@nestjs/jwt";
+
+import { PrismaService } from "~/prisma.service";
+import { extractTokenFromHeader } from "~/utils/string";
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate {
+    constructor(
+        private jwtService: JwtService,
+        private prisma: PrismaService,
+    ) {}
+
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
-        const token = this.extractTokenFromHeader(request);
-
-        const secret = process.env.ADMIN_PANEL_SECRET;
-
-        if (!secret || secret == "") {
-            throw new InternalServerErrorException("Secret for admin panel is not set");
+        const token = extractTokenFromHeader(request);
+        if (!token) {
+            throw new UnauthorizedException();
         }
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: process.env.JWT_SECRET,
+            });
 
-        if (!token || token !== process.env.ADMIN_PANEL_SECRET) {
+            const user = await this.prisma.user.findFirstOrThrow({ where: { id: payload.uId } });
+
+            if (user.banned) {
+                throw new ForbiddenException("This user is banned");
+            }
+
+            if (!user.admin) {
+                throw new UnauthorizedException("This user is not admin");
+            }
+
+            request["user"] = user;
+        } catch {
             throw new UnauthorizedException("Could not authenticate");
         }
         return true;
