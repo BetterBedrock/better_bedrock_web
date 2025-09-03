62da492851c2d6ffa1d99d624f1fdf6423b6f02a
iDarkQ <matikuki3@gmail.com>
2025-09-03 19:19:02 +0100

    refactor(create-project.dto.ts): include userid in create project dto

--- apps/api/src/project/dto/create-project.dto.ts
+++ apps/api/src/project/dto/create-project.dto.ts
@@ -1,4 +1,4 @@
 import { PickType } from "@nestjs/swagger";
 import { ProjectDto } from "~/project/dto/project.dto";
 
-export class CreateProjectDto extends PickType(ProjectDto, ["title"] as const) {}
+export class CreateProjectDto extends PickType(ProjectDto, ["title", "userId"] as const) {}
