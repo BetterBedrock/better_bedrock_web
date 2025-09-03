dab57fd0790e2911c1392acf3d41ce546970083f
iDarkQ <matikuki3@gmail.com>
2025-09-03 19:23:27 +0100

    refactor(project-owner.guard.ts): return detailed project

--- apps/api/src/project/guards/project-owner.guard.ts
+++ apps/api/src/project/guards/project-owner.guard.ts
@@ -4,35 +4,48 @@ import {
     ForbiddenException,
     Injectable,
     BadRequestException,
+    NotFoundException,
 } from "@nestjs/common";
+import { ProjectRequest } from "~/common/types/project-request.type";
 import { PrismaService } from "~/prisma.service";
+import { projectCreatorInclude, projectTagsInclude } from "~/project/project.service";
+import { RatingService } from "~/rating/rating.service";
 
 @Injectable()
 export class ProjectOwnerGuard implements CanActivate {
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly ratingService: RatingService,
+    ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const req = context.switchToHttp().getRequest();
-        const user = req.user;
-        const projectId = req.params.id;
+        const req = context.switchToHttp().getRequest<ProjectRequest>();
+        const { user, params } = req;
+        const projectId = params?.id;
 
-        if (!projectId) {
-            throw new BadRequestException("Project ID is required");
-        }
+        if (!projectId) throw new BadRequestException("Project ID is required");
 
-        const project = await this.prisma.project.findUnique({
+        const project = await this.prismaService.project.findUnique({
             where: { id_draft: { id: projectId, draft: true } },
+            include: { ...projectCreatorInclude, ...projectTagsInclude },
         });
 
-        if (!project) {
-            throw new BadRequestException("Project not found");
-        }
-
-        if (project.userId !== user.id) {
+        if (!project) throw new NotFoundException("Project not found");
+        if (project.userId !== user.id && !user.admin) {
             throw new ForbiddenException("You do not own this project");
         }
 
-        req.project = project;
+        const [projectRating, userRating] = await Promise.all([
+            this.ratingService.getProjectRating(project.id),
+            this.ratingService.getProfileRating(project.userId),
+        ]);
+
+        req.project = {
+            ...project,
+            rating: projectRating,
+            user: { ...project.user, rating: userRating.average },
+        };
+
         return true;
     }
 }
