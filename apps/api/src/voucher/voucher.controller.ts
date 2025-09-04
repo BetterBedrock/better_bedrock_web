3435cdb08e0146fc1a2cbd103993c8077e62f664
iDarkQ <matikuki3@gmail.com>
2025-09-04 19:26:27 +0100

    refactor(voucher): utilize best practices, remove await where not needed & simplify code

--- apps/api/src/voucher/voucher.controller.ts
+++ apps/api/src/voucher/voucher.controller.ts
@@ -1,52 +1,34 @@
 import { Body, Controller, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
-import { ApiTags, ApiOkResponse, ApiBearerAuth } from "@nestjs/swagger";
+import { ApiBearerAuth } from "@nestjs/swagger";
 import { AdminAuthGuard } from "~/auth/admin-auth.guard";
 import { CreateVoucher } from "~/voucher/dto/create-voucher.dto";
 import { UpdateVoucher } from "~/voucher/dto/update-voucher.dto";
 import { VoucherDto } from "~/voucher/dto/voucher.dto";
 import { VoucherService } from "~/voucher/voucher.service";
 
-@ApiTags("voucher")
 @Controller("voucher")
+@UseGuards(AdminAuthGuard)
+@ApiBearerAuth()
 export class VoucherController {
     constructor(private voucherService: VoucherService) {}
 
     @Get()
-    @ApiOkResponse({
-        description: "Returns list of existing voucher",
-        type: VoucherDto,
-        isArray: true,
-    })
-    @ApiBearerAuth()
-    @UseGuards(AdminAuthGuard)
     async vouchers(): Promise<VoucherDto[]> {
-        return await this.voucherService.getVouchers();
+        return this.voucherService.getVouchers();
     }
 
     @Post()
-    @ApiOkResponse({
-        description: "Returns newly created voucher",
-        type: VoucherDto,
-    })
-    @ApiBearerAuth()
-    @UseGuards(AdminAuthGuard)
     async create(@Body() data: CreateVoucher): Promise<VoucherDto> {
         if (data.code == "" || !data.code) {
             data.code = this.voucherService.generateCode(8);
         }
 
-        return await this.voucherService.createVoucher(data);
+        return this.voucherService.createVoucher(data);
     }
 
     @Patch(":id")
-    @ApiOkResponse({
-        description: "Returns updated voucher",
-        type: VoucherDto,
-    })
-    @ApiBearerAuth()
-    @UseGuards(AdminAuthGuard)
     async update(@Param("id") id: string, @Body() data: UpdateVoucher): Promise<VoucherDto> {
-        return await this.voucherService.updateVoucher({
+        return this.voucherService.updateVoucher({
             where: { id: id },
             data,
         });
