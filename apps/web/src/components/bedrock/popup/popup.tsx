3de674028e3fb794f527b841516b5f745377d434
iDarkQ <matikuki3@gmail.com>
2025-09-04 19:30:49 +0100

    refactor(popup): utilize simple button for exit

--- apps/web/src/components/bedrock/popup/popup.tsx
+++ apps/web/src/components/bedrock/popup/popup.tsx
@@ -3,6 +3,7 @@ import { styles } from ".";
 import clsx from "clsx";
 import Exit from "~/assets/images/exit.png";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
+import { SimpleButton } from "~/components/bedrock/simple-button";
 
 interface PopupProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
@@ -18,9 +19,17 @@ export const Popup = ({ children, onClose, className, title }: PopupProps) => (
       onClick={(e) => e.stopPropagation()}
     >
       <div className={styles.header}>
-        <img alt="Close" src={Exit} className={styles.close} onClick={onClose} />
+        <SimpleButton onClick={onClose} transparent className={styles.close}>
+          <img alt="Close" src={Exit} className={styles.icon} />
+        </SimpleButton>
         {title && (
-          <BedrockText text={title} font="MinecraftTen" type="h2" color="white" textAlign="center" />
+          <BedrockText
+            text={title}
+            font="MinecraftTen"
+            type="h2"
+            color="white"
+            textAlign="center"
+          />
         )}
       </div>
       {children}
