24bee3892d838809f95b03e42bbd54f6c4680c0d
iDarkQ <matikuki3@gmail.com>
2025-09-05 19:36:51 +0100

    refactor(hero-title.tsx): simplify hero title

--- apps/web/src/pages/preview/components/hero/hero-title.tsx
+++ apps/web/src/pages/preview/components/hero/hero-title.tsx
@@ -1,12 +1,9 @@
 import { BedrockText } from "~/components/bedrock/bedrock-text";
-import { styles } from ".";
 
 interface HeroTitleProps {
   title: string;
 }
 
 export const HeroTitle = ({ title }: HeroTitleProps) => (
-  <div className={styles.title}>
-    <BedrockText text={title} type="h1" textAlign="start" color="white" font="MinecraftTen" />
-  </div>
+  <BedrockText text={title} type ="h1" textAlign="start" color="white" font="MinecraftTen" />
 );
