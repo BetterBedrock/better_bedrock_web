32d856b12a1fc87947b3e92e2514c48b877b51f4
iDarkQ <matikuki3@gmail.com>
2025-09-08 11:03:01 +0100

    refactor(project-tag.dto.ts): set min and max length of tag name

--- apps/api/src/project/dto/project-tag.dto.ts
+++ apps/api/src/project/dto/project-tag.dto.ts
@@ -1,7 +1,9 @@
-import { IsBoolean, IsString } from "class-validator";
+import { IsBoolean, IsString, MaxLength, MinLength } from "class-validator";
 
 export class ProjectTagDto {
     @IsString()
+    @MinLength(1, { message: "Each tag cannot be shorter than 1 characters" })
+    @MaxLength(15, { message: "Tag '$value' cannot be longer than 15 characters" })
     name: string;
     @IsString()
     projectId: string;
