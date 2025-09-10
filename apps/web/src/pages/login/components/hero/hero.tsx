fb35fd6738cede9200f6f2d9e6fc983dca1ec49c
iDarkQ <matikuki3@gmail.com>
2025-09-10 11:39:24 +0100

    style(hero.tsx): fix spacings

--- apps/web/src/pages/login/components/hero/hero.tsx
+++ apps/web/src/pages/login/components/hero/hero.tsx
@@ -2,8 +2,8 @@ import { HeroHeader, HeroActions } from ".";
 import { styles } from ".";
 
 export const Hero = () => (
-    <div className={styles.wrapper}>
-      <HeroHeader />
-      <HeroActions />
-    </div>
-  );
+  <div className={styles.wrapper}>
+    <HeroHeader />
+    <HeroActions />
+  </div>
+);
