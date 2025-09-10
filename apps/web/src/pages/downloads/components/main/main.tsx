ba2a7deaa9bbec06594bdb1d66cb218cf7acbaa6
iDarkQ <matikuki3@gmail.com>
2025-09-10 11:38:31 +0100

    refactor(main): move projects into a seperate card & add banner

--- apps/web/src/pages/downloads/components/main/main.tsx
+++ apps/web/src/pages/downloads/components/main/main.tsx
@@ -3,18 +3,37 @@ import { styles } from ".";
 import { MainAd } from "~/pages/downloads/components/main/main-ad";
 import { MainProjects } from "~/pages/downloads/components/main/main-projects";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
+import { Banner } from "~/components/bedrock/banner";
 
 export const Main = () => {
   return (
-    <Card sub className={styles.main}>
-      <div className={styles.downloads}>
-        <BedrockText text="Downloads" type="h1" color="white" font="Minecraft" textAlign="start" />
-      </div>
-      <CardDivider sub />
-      <div className={styles.downloads}>
-        <MainAd />
-        <MainProjects />
-      </div>
-    </Card>
+    <>
+      <Card sub className={styles.main}>
+        <div className={styles.downloads}>
+          <BedrockText
+            text="Downloads"
+            type="h1"
+            color="white"
+            font="Minecraft"
+            textAlign="start"
+          />
+        </div>
+        <CardDivider sub />
+        <div className={styles.downloads}>
+          <MainAd />
+        </div>
+      </Card>
+      <Card sub className={styles.main}>
+        <div className={styles.downloads}>
+          <Banner type="info" message="Our creators get 100% of the ad revenue!" />{" "}
+      
+          <BedrockText text="Projects" type="h1" color="white" font="Minecraft" textAlign="start" />
+        </div>
+        <CardDivider sub />
+        <div className={styles.downloads}>
+          <MainProjects />
+        </div>
+      </Card>
+    </>
   );
 };
