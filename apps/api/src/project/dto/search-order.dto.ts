0a127b15834e514030f7e91dc5081cf53c90114f
iDarkQ <matikuki3@gmail.com>
2025-09-16 07:55:39 +0100

    feat(search-order.dto.ts): create enum for search order

---
+++ apps/api/src/project/dto/search-order.dto.ts
@@ -0,0 +1,7 @@
+export enum SearchOrder {
+    newest = "Newest",
+    oldest = "Oldest",
+    mostPopularThisWeek = "Most Popular This Week",
+    mostPopularThisMonth = "Most Popular This Month",
+    mostPopularThisYear = "Most Popular This Year",
+}
