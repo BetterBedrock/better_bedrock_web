f91e1aa192cd9b23aae9c5586d6eaa7bdf88e9ac
iDarkQ <matikuki3@gmail.com>
2025-09-19 10:51:58 +0100

    fix(popup.tsx): spacings

--- apps/web/src/components/bedrock/popup/popup.tsx
+++ apps/web/src/components/bedrock/popup/popup.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import Exit from "~/assets/images/exit.png";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 import { SimpleButton } from "~/components/bedrock/simple-button";
+import { ReactNode } from "react";
 
 interface PopupProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
@@ -23,13 +24,7 @@ export const Popup = ({ children, onClose, className, title }: PopupProps) => (
           <img alt="Close" src={Exit} className={styles.icon} />
         </SimpleButton>
         {title && (
-          <BedrockText
-            text={title}
-            font="Minecraft"
-            type="h2"
-            color="white"
-            textAlign="center"
-          />
+          <BedrockText text={title} font="Minecraft" type="h2" color="white" textAlign="center" />
         )}
       </div>
       {children}
