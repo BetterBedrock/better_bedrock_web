b0d9476c0a0dc16d9e190ae0ca3394d4bcb5b331
iDarkQ <matikuki3@gmail.com>
2025-09-20 11:03:52 +0100

    feat(gallery-button.tsx): create new toolbar button

---
+++ apps/web/src/components/text-editor/ui/gallery/gallery-button.tsx
@@ -0,0 +1,34 @@
+// src/components/text-editor/ui/gallery-button.tsx
+import React, { useContext } from "react";
+import { EditorContext } from "@tiptap/react";
+import ImageGalleryIcon from "~/assets/ui/tiptap-icons/18.png";
+import { ToolbarButton } from "~/components/text-editor/primitive/toolbar-button";
+import { TiptapIcon } from "~/components/text-editor/tiptap-icon";
+
+export const GalleryButton: React.FC = () => {
+  const { editor } = useContext(EditorContext);
+
+  const handleCreateGallery = () => {
+    if (!editor) return;
+    editor.chain().focus().setGallery({ images: [] }).run();
+  };
+
+  const isActive = !editor?.can().setGallery({ images: [] });
+
+  return (
+    <>
+      <ToolbarButton
+        type="button"
+        data-active-state={isActive ? "on" : "off"}
+        role="button"
+        tabIndex={-1}
+        aria-pressed={isActive}
+        onClick={handleCreateGallery}
+        tooltip="Add gallery"
+      >
+        <TiptapIcon icon={ImageGalleryIcon} className="tiptap-button-icon" />
+        <span className="tiptap-button-text">Gallery</span>
+      </ToolbarButton>
+    </>
+  );
+};
