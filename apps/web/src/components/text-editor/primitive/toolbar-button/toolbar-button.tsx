55b1a532c3a8c1eafda6dd97c6b80e0c44ccd6f2
iDarkQ <matikuki3@gmail.com>
2025-09-20 11:03:02 +0100

    refactor(toolbar-button): always display tooltip

--- apps/web/src/components/text-editor/primitive/toolbar-button/toolbar-button.tsx
+++ apps/web/src/components/text-editor/primitive/toolbar-button/toolbar-button.tsx
@@ -6,45 +6,23 @@ import { forwardRef } from "react";
 
 export interface ToolbarButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  showTooltip?: boolean;
   tooltip?: React.ReactNode;
 }
 
 export const ToolbarButton = forwardRef<HTMLButtonElement, ToolbarButtonProps>(
-  (
-    { className, children, tooltip, showTooltip = true, "aria-label": ariaLabel, ...props },
-    ref,
-  ) => {
-    if (!tooltip || !showTooltip) {
-      return (
-        <SimpleButton
-          transparent
-          className={clsx(styles.button, className)}
-          ref={ref}
-          aria-label={ariaLabel}
-          isClicked={Boolean(props["aria-pressed"])}
-          {...props}
-        >
-          {children}
-        </SimpleButton>
-      );
-    }
-
-    // console.log({ pres: Boolean(props["aria-pressed"]), tpp: tooltip });
-
-    return (
-      <Tooltip text={String(tooltip)}>
-        <SimpleButton
-          transparent
-          className={clsx(styles.button, className)}
-          ref={ref}
-          aria-label={ariaLabel}
-          isClicked={Boolean(props["aria-pressed"])}
-          {...props}
-        >
-          {children}
-        </SimpleButton>
-      </Tooltip>
-    );
-  },
+  ({ className, children, tooltip, "aria-label": ariaLabel, ...props }, ref) => (
+    <Tooltip text={String(tooltip)}>
+      <SimpleButton
+        transparent
+        className={clsx(styles.button, className)}
+        extraClassName={styles.child}
+        ref={ref}
+        aria-label={ariaLabel}
+        isClicked={Boolean(props["aria-pressed"])}
+        {...props}
+      >
+        {children}
+      </SimpleButton>
+    </Tooltip>
+  ),
 );
