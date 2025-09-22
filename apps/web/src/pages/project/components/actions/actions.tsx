f3b8b83527036de7bb0621915fc16c72545279d6
iDarkQ <matikuki3@gmail.com>
2025-09-22 11:10:50 +0100

    refactor(actions): move all functions that manage project to the popup

--- apps/web/src/pages/project/components/actions/actions.tsx
+++ apps/web/src/pages/project/components/actions/actions.tsx
@@ -1,28 +1,19 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 import { Button } from "~/components/bedrock/button";
 import { PopupConfirmation } from "~/components/bedrock/popup/popup-confirmation";
+import { PopupWrapper } from "~/components/bedrock/popup/popup-wrapper";
 import { ButtonGroup } from "~/components/button-group/button-group";
+import { ActionsDeletePopup } from "~/pages/project/components/actions/actions-delete-popup";
 import { ActionsSave } from "~/pages/project/components/actions/actions-save";
 import { useProjectManager } from "~/pages/project/providers/project-manager";
-import { useNotification } from "~/providers/notification";
 import { useProject } from "~/providers/project";
-import { Routes } from "~/utils/routes";
 
 export const Actions = () => {
-  const navigate = useNavigate();
-  const { sendNotification } = useNotification();
   const {
     submitProject,
     cancelSubmission,
-    deleteProject,
-    deleteProductionProject,
-    fetchProjectDetails,
   } = useProject();
   const { handleSaveProject, selectedProject, setSelectedProject } = useProjectManager();
-
-  const [existsProductionProject, setExistsProductionProject] = useState(false);
 
   const handleSubmission = async () => {
     if (!selectedProject) return;
@@ -39,30 +30,6 @@ export const Actions = () => {
     }
   };
 
-  const handleDelete = async (publicOnly?: boolean) => {
-    if (!selectedProject) return;
-
-    if (publicOnly) {
-      await deleteProductionProject(selectedProject.id, selectedProject.title);
-    } else {
-      await deleteProject(selectedProject.id, selectedProject.title);
-    }
-
-    sendNotification({
-      title: "Deleted",
-      label: "Successfully deleted project",
-      type: "success",
-    });
-
-    navigate(Routes.HOME);
-  };
-
-  useEffect(() => {
-    fetchProjectDetails(selectedProject!.id, false).then((data) =>
-      setExistsProductionProject(!!data),
-    );
-  }, []);
-
   return (
     <ButtonGroup>
       <ActionsSave onClick={async () => await handleSaveProject(selectedProject!)} />
@@ -72,36 +39,21 @@ export const Actions = () => {
         confirmText="Submit"
         ignore={selectedProject?.submitted}
       >
-        <Button width="100%" type="dark" onClick={handleSubmission} center>
+        <Button width="100%" height="100%" type="white" onClick={handleSubmission} center>
           <BedrockText
             text={selectedProject?.submitted ? "Cancel Submission" : "Submit for review"}
             type="p"
-            color="white"
+            color="black"
           />
         </Button>
       </PopupConfirmation>
 
-      <PopupConfirmation
-        description="Are you sure you want to delete this project? Both the released and draft versions will be deleted."
-        confirmText="Delete"
-        confirmType="red"
-      >
-        <Button width="100%" type="red" center onClick={() => handleDelete(false)}>
-          <BedrockText text={"Delete Entire Project"} type="p" color="white" />
+      <PopupWrapper popup={(close) => <ActionsDeletePopup close={close} />}>
+        <Button width="100%" height="100%" type="red" center>
+          <BedrockText text={"Deletion Options"} type="p" color="white" />
         </Button>
-      </PopupConfirmation>
+      </PopupWrapper>
 
-      {existsProductionProject && (
-        <PopupConfirmation
-          description="Are you sure you want to delete this project? Only public version of this project will be deleted. The draft and statistics will remain untouched."
-          confirmText="Delete"
-          confirmType="red"
-        >
-          <Button width="100%" type="red" center onClick={() => handleDelete(true)}>
-            <BedrockText text={"Delete Public Project"} type="p" color="white" />
-          </Button>
-        </PopupConfirmation>
-      )}
     </ButtonGroup>
   );
 };
---
+++ apps/web/src/pages/project/components/actions/index.ts
@@ -0,0 +1,4 @@
+export * from "./actions";
+export * from "./actions-delete-popup";
+export * from "./actions-save";
+export { default as styles } from "./actions.module.scss";
