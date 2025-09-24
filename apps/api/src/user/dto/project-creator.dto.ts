95fd602a4d0473b034ddf30485420a30909ad762
iDarkQ <matikuki3@gmail.com>
2025-09-24 09:08:08 +0100

    refactor(project-creator.dto.ts): set min and max rating user can give to a project

--- apps/api/src/user/dto/project-creator.dto.ts
+++ apps/api/src/user/dto/project-creator.dto.ts
@@ -1,8 +1,10 @@
 import { PickType } from "@nestjs/swagger";
-import { IsNumber } from "class-validator";
+import { IsNumber, Max, Min } from "class-validator";
 import { UserDto } from "~/user/dto/user.dto";
 
 export class ProjectCreatorDto extends PickType(UserDto, ["name"] as const) {
     @IsNumber()
+    @Min(0)
+    @Max(5)
     rating: number;
 }
