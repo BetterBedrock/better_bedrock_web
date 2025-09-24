885081191189338da59786ce6f6671ac0ae6b3c2
iDarkQ <matikuki3@gmail.com>
2025-09-24 09:11:43 +0100

    refactor(vouchers): change voucher folder name to vouchers

--- apps/web/src/pages/panel/vouchers/components/hero/voucher-form/voucher-form.tsx
+++ apps/web/src/pages/panel/vouchers/components/hero/voucher-form/voucher-form.tsx
@@ -1,26 +1,25 @@
 import { BedrockText } from "~/components/bedrock/bedrock-text";
 import { Popup } from "~/components/bedrock/popup";
 import { CreateVoucher, VoucherDto } from "~/lib/api";
-import { styles, useVoucherForm } from ".";
+import { useVoucherForm } from ".";
 import { CardDivider } from "~/components/bedrock/card";
 import { Button } from "~/components/bedrock/button";
-import { VoucherFormField } from "~/pages/panel/voucher/components/hero/voucher-form/voucher-form-field";
+import { VoucherFormField } from "~/pages/panel/vouchers/components/hero/voucher-form/voucher-form-field";
 
 interface VoucherFormProps {
-  open: boolean;
   voucher?: VoucherDto;
   onClose: () => void;
   onSubmit: (voucher: CreateVoucher) => void;
 }
 
-export const VoucherForm = ({ open, onClose, voucher, onSubmit }: VoucherFormProps) => {
+export const VoucherForm = ({ onClose, voucher, onSubmit }: VoucherFormProps) => {
   const { onClickSubmit, fields, control, errors } = useVoucherForm({ voucher, onSubmit });
 
   return (
-    open && (
-      <Popup title={voucher ? "Edit Voucher" : "Create Voucher"} onClose={onClose}>
-        <form onSubmit={onClickSubmit} className={styles.form}>
-          <div className={styles.part}>
+    <Popup title={voucher ? "Edit Voucher" : "Create Voucher"} onClose={onClose}>
+      <Popup.Wrapper>
+        <form onSubmit={onClickSubmit}>
+          <Popup.Part>
             {fields.map(({ name, label, type, placeholder }) => (
               <VoucherFormField
                 errors={errors}
@@ -31,16 +30,16 @@ export const VoucherForm = ({ open, onClose, voucher, onSubmit }: VoucherFormPro
                 placeholder={placeholder}
               />
             ))}
-          </div>
+          </Popup.Part>
 
           <CardDivider />
-          <div className={styles.part}>
+          <Popup.Part>
             <Button buttonType="submit" type="green" center>
               <BedrockText color="white" type="p" text="Confirm" />
             </Button>
-          </div>
+          </Popup.Part>
         </form>
-      </Popup>
-    )
+      </Popup.Wrapper>
+    </Popup>
   );
 };
---
+++ apps/web/src/pages/panel/vouchers/index.ts
@@ -0,0 +1,2 @@
+export * from "./vouchers";
+export { default as styles } from "./vouchers.module.scss";
