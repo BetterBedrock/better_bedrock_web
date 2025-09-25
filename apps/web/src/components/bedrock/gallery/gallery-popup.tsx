cd96024b150357f52830a9a875bea41bc859f277
iDarkQ <matikuki3@gmail.com>
2025-09-25 09:24:36 +0100

    feat(gallery-popup.tsx): create popup component

---
+++ apps/web/src/components/bedrock/gallery/gallery-popup.tsx
@@ -0,0 +1,90 @@
+import { BedrockText } from "~/components/bedrock/bedrock-text";
+import { Button } from "~/components/bedrock/button";
+import { CardDivider } from "~/components/bedrock/card";
+import { Popup } from "~/components/bedrock/popup";
+import { BasePopupWrapperProps } from "~/components/bedrock/popup/popup-wrapper";
+import { SimpleButton } from "~/components/bedrock/simple-button";
+import { styles } from ".";
+import { baseUrl } from "~/utils/url";
+import { useRef } from "react";
+import Exit from "~/assets/images/exit.png";
+
+interface GalleryPopupProps extends BasePopupWrapperProps {
+  images: string[];
+  onDeleteImage?: (index: number) => void;
+  onAddImages?: (files: FileList) => void;
+  maxImages?: number;
+}
+
+export const GalleryPopup = ({
+  close,
+  images,
+  onAddImages,
+  onDeleteImage,
+  maxImages = 10,
+}: GalleryPopupProps) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleAddClick = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+  const handleFilesSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    onAddImages?.(e.target.files);
+    e.target.value = "";
+  };
+
+  return (
+    <Popup title="Edit Gallery" onClose={close}>
+      <Popup.Wrapper>
+        {images.length > 0 && (
+          <>
+            <Popup.Part>
+              <div className={styles.list}>
+                {images.map((src, idx) => (
+                  <div key={src} className={styles.wrapper}>
+                    <img
+                      src={baseUrl + "/" + src}
+                      alt={`Edit ${idx + 1}`}
+                      className={styles.image}
+                    />
+                    <SimpleButton
+                      onClick={() => onDeleteImage?.(idx)}
+                      transparent
+                      className={styles.delete}
+                      width="100%"
+                      height="100%"
+                    >
+                      <img alt="Close" src={Exit} className={styles.icon} />
+                    </SimpleButton>
+                  </div>
+                ))}
+              </div>
+            </Popup.Part>
+            <CardDivider />
+          </>
+        )}
+        <Popup.Part>
+          <input
+            ref={fileInputRef}
+            type="file"
+            multiple
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleFilesSelected}
+          />
+          <Button
+            type={images.length >= maxImages ? "dark" : "green"}
+            center
+            onClick={handleAddClick}
+          >
+            <BedrockText
+              text={`Add Images (${images.length} / ${maxImages})`}
+              type="p"
+              color="white"
+            />
+          </Button>
+        </Popup.Part>
+      </Popup.Wrapper>
+    </Popup>
+  );
+};
