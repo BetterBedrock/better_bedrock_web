3eabfade6ac3cefa139365b8df68d7e6b422413b
iDarkQ <matikuki3@gmail.com>
2025-09-27 09:55:13 +0100

    refactor(fetch.tsx): wrap fetch children with download provider

--- apps/web/src/pages/fetch/fetch.tsx
+++ apps/web/src/pages/fetch/fetch.tsx
@@ -1,11 +1,14 @@
 import { Section } from "~/components/section";
 import { Hero } from "./components/hero";
 import { styles } from ".";
+import { DownloadProvider } from "~/providers/download";
 
 export const Fetch = () => (
   <main>
     <Section className={styles.background} extraClassName={styles.padding} center fixed>
-      <Hero />
+      <DownloadProvider>
+        <Hero />
+      </DownloadProvider>
     </Section>
   </main>
 );
