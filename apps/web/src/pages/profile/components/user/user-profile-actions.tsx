3caacb9abfab9ab0a41028296514bf295df99876
iDarkQ <matikuki3@gmail.com>
2025-09-29 10:32:07 +0100

    refactor(user): move popups to profile actions & utilize popupwrapper to display them

--- apps/web/src/pages/profile/components/user/user-profile-actions.tsx
+++ apps/web/src/pages/profile/components/user/user-profile-actions.tsx
@@ -1,25 +1,55 @@
 import { SimpleButton } from "~/components/bedrock/simple-button";
 import SettingsGlyph from "~/assets/images/settings_glyph.png";
 import ReportGlyph from "~/assets/images/glyphs/WarningGlyph.png";
-import { styles } from ".";
+import { styles, UserSettingsForm } from ".";
 import { useUserProfile } from "~/pages/profile/providers/user-profile";
+import { PopupWrapper } from "~/components/bedrock/popup/popup-wrapper";
+import { PopupReport } from "~/components/bedrock/popup/popup-report";
 
 export const UserProfileActions = () => {
-  const { user, selectedUser, detailedSelectedUser, setSettingsOpen, setReportOpen } =
-    useUserProfile();
+  const {
+    user,
+    selectedUser,
+    detailedSelectedUser,
+    setSettingsOpen,
+    ownsProfile,
+    handleProfileSave,
+  } = useUserProfile();
   const canRate = user && user?.name !== selectedUser!.name;
 
   return (
     <div className={styles.icons}>
       {canRate && (
-        <SimpleButton transparent onClick={() => setReportOpen(true)}>
-          <img src={ReportGlyph} className={styles.report} />
-        </SimpleButton>
+        <PopupWrapper
+          popup={(close) => (
+            <PopupReport
+              name={selectedUser!.name}
+              id={selectedUser!.id}
+              type="user"
+              onClose={close}
+            />
+          )}
+        >
+          <SimpleButton transparent>
+            <img src={ReportGlyph} className={styles.report} />
+          </SimpleButton>
+        </PopupWrapper>
       )}
       {detailedSelectedUser && (
-        <SimpleButton transparent onClick={() => setSettingsOpen(true)}>
-          <img src={SettingsGlyph} className={styles.settings} />
-        </SimpleButton>
+        <PopupWrapper
+          popup={(close) => (
+            <UserSettingsForm
+              user={detailedSelectedUser}
+              onClose={close}
+              admin={!ownsProfile && user?.admin}
+              onSave={handleProfileSave}
+            />
+          )}
+        >
+          <SimpleButton transparent onClick={() => setSettingsOpen(true)}>
+            <img src={SettingsGlyph} className={styles.settings} />
+          </SimpleButton>
+        </PopupWrapper>
       )}
     </div>
   );
--- apps/web/src/pages/profile/components/user/user.tsx
+++ apps/web/src/pages/profile/components/user/user.tsx
@@ -1,39 +1,39 @@
-import { PopupReport } from "~/components/bedrock/popup/popup-report";
-import { UserCard, UserSettingsForm } from ".";
+import { BedrockText } from "~/components/bedrock/bedrock-text";
+import { Card } from "~/components/bedrock/card";
+import {
+  styles,
+  UserProfileActions,
+  UserProfileName,
+  UserProfilePicture,
+  UserProfileRating,
+} from ".";
 import { useUserProfile } from "~/pages/profile/providers/user-profile";
+import { UserLinkvertiseInfo } from "~/pages/profile/components/user/user-linkvertise-info";
+import { useAuth } from "~/providers/auth";
 
 export const User = () => {
-  const {
-    selectedUser,
-    settingsOpen,
-    setSettingsOpen,
-    reportOpen,
-    setReportOpen,
-    detailedSelectedUser,
-    ownsProfile,
-    user,
-    handleProfileSave,
-  } = useUserProfile();
+  const { selectedUser, rating } = useUserProfile();
+  const { user } = useAuth();
 
   return (
     <>
-      {settingsOpen && detailedSelectedUser && (
-        <UserSettingsForm
-          user={detailedSelectedUser}
-          onClose={() => setSettingsOpen(false)}
-          admin={!ownsProfile && user?.admin}
-          onSave={handleProfileSave}
-        />
-      )}
-      {reportOpen && selectedUser && (
-        <PopupReport
-          name={selectedUser.name}
-          id={selectedUser.id}
-          type="user"
-          onClose={() => setReportOpen(false)}
-        />
-      )}
-      <UserCard />
+      {user?.id === selectedUser?.id && !selectedUser?.linkvertiseId && <UserLinkvertiseInfo />}
+      <Card className={styles.card}>
+        <div className={styles.profile}>
+          <UserProfilePicture name={selectedUser!.name} />
+          <div className={styles.info}>
+            <div className={styles.header}>
+              <UserProfileName name={selectedUser!.name} banned={selectedUser!.banned} />
+              <UserProfileActions />
+            </div>
+            <UserProfileRating rating={rating} />
+            <BedrockText type="p" text={selectedUser!.bio} color="white" textAlign="left" extraClassName={styles.text} />
+          </div>
+        </div>
+        <div className={styles.bio}>
+          <BedrockText type="p" text={selectedUser!.bio} color="white" textAlign="left" />
+        </div>
+      </Card>
     </>
   );
 };
