83783421d83d0b6c8483f1fd4f123cb0bf6d44f7
iDarkQ <matikuki3@gmail.com>
2025-09-29 10:34:57 +0100

    refactor(user-settings-form.tsx): replace dvis with popup sub components

--- apps/web/src/pages/profile/components/user/user-settings-form.tsx
+++ apps/web/src/pages/profile/components/user/user-settings-form.tsx
@@ -57,140 +57,152 @@ export const UserSettingsForm = ({
 
   return (
     <Popup onClose={onClose} title="Account Settings">
-      <form onSubmit={onClickSubmit}>
-        <div className={styles.part}>
-          <div className={styles.input}>
-            <BedrockText textAlign="start" text="Username" type="p" color="white" />
-            <Input placeholder="Name" className={styles.input} {...register("name")} />
-            {errors["name"] && (
-              <BedrockText
-                type="p2"
-                extraClassName={styles.error}
-                text={errors["name"]?.message as string}
-                textAlign="start"
-              />
-            )}
-          </div>
-
-          <div className={styles.input}>
-            <BedrockText textAlign="start" text="Bio" type="p" color="white" />
-            <Input
-              placeholder="Profile description..."
-              className={styles.input}
-              {...register("bio")}
-            />
-
-            {errors["bio"] && (
-              <BedrockText
-                type="p2"
-                extraClassName={styles.error}
-                text={errors["bio"]?.message as string}
-                textAlign="start"
-              />
-            )}
-          </div>
-
-          <div>
-            <BedrockText
-              textAlign="start"
-              text="Custom Linkvertise (allows you to receive 100% of ad money to your account)"
-              type="p"
-              color="white"
-            />
-            <Link link={Routes.HOME}>
-              <BedrockText
-                textAlign="start"
-                text="Learn how to make money with your projects"
-                type="p2"
-                extraClassName={styles.link}
-              />
-            </Link>
-            <Controller
-              name="customLinkvertise"
-              control={control}
-              render={({ field }) => (
-                <InputSwitch
-                  placeholder="Custom Linkvertise"
-                  checked={field.value}
-                  onChange={(e) => {
-                    field.onChange(e.target.checked);
-                    setShowLinkvertiseOptions(e.target.checked);
-                  }}
+      <Popup.Wrapper>
+        <form onSubmit={onClickSubmit}>
+          <Popup.Part>
+            <div className={styles.input}>
+              <BedrockText textAlign="start" text="Username" type="p" color="white" />
+              <Input placeholder="Name" className={styles.input} {...register("name")} />
+              {errors["name"] && (
+                <BedrockText
+                  type="p2"
+                  extraClassName={styles.error}
+                  text={errors["name"]?.message as string}
+                  textAlign="start"
                 />
               )}
-            />
-          </div>
+            </div>
 
-          {showLinkvertiseOptions && (
-            <>
-              <div className={styles.input}>
-                <BedrockText textAlign="start" text="Linkvertise User Id" type="p" color="white" />
-                <Input
-                  placeholder="Linkvertise User Id"
-                  className={styles.input}
-                  {...register("linkvertiseId")}
+            <div className={styles.input}>
+              <BedrockText textAlign="start" text="Bio" type="p" color="white" />
+              <Input
+                placeholder="Profile description..."
+                className={styles.input}
+                {...register("bio")}
+              />
+
+              {errors["bio"] && (
+                <BedrockText
+                  type="p2"
+                  extraClassName={styles.error}
+                  text={errors["bio"]?.message as string}
+                  textAlign="start"
                 />
-
-                {errors["linkvertiseId"] && (
-                  <BedrockText
-                    type="p2"
-                    extraClassName={styles.error}
-                    text={errors["linkvertiseId"]?.message as string}
-                    textAlign="start"
-                  />
-                )}
-              </div>
-              <div className={styles.input}>
-                <BedrockText textAlign="start" text="Linkvertise Anti-bypass Token" type="p" color="white" />
-                <Input
-                  placeholder="Linkvertise Anti-bypass Token"
-                  className={styles.input}
-                  type="password"
-                  {...register("linkvertiseSecret")}
-                />
-
-                {errors["linkvertiseSecret"] && (
-                  <BedrockText
-                    type="p2"
-                    extraClassName={styles.error}
-                    text={errors["linkvertiseSecret"]?.message as string}
-                    textAlign="start"
-                  />
-                )}
-              </div>
-            </>
-          )}
-        </div>
-
-        <CardDivider />
-        <div className={styles.part}>
-          {admin && (
-            <Controller
-              name="banned"
-              control={control}
-              render={({ field }) => (
-                <Button
-                  type={field.value ? "dark" : "red"}
-                  width="100%"
-                  center
-                  onClick={() => field.onChange(!field.value)}
-                  buttonType="submit"
-                >
-                  <BedrockText type="p" text={field.value ? "Unban" : "Ban"} color="white" />
-                </Button>
               )}
-            />
-          )}
-          <Button type="green" buttonType="submit" center width="100%">
-            <BedrockText type="p" text="Save Settings" color="white" />
-          </Button>
-          {!admin && (
-            <Button type="dark" width="100%" center onClick={logout}>
-              <BedrockText type="p" text="Logout" color="white" />
+            </div>
+
+            <div>
+              <BedrockText
+                textAlign="start"
+                text="Custom Linkvertise (allows you to receive 100% of ad money to your account)"
+                type="p"
+                color="white"
+              />
+              <Link link={Routes.HOME}>
+                <BedrockText
+                  textAlign="start"
+                  text="Learn how to make money with your projects"
+                  type="p2"
+                  extraClassName={styles.link}
+                />
+              </Link>
+              <Controller
+                name="customLinkvertise"
+                control={control}
+                render={({ field }) => (
+                  <InputSwitch
+                    placeholder="Custom Linkvertise"
+                    checked={field.value}
+                    onChange={(e) => {
+                      field.onChange(e.target.checked);
+                      setShowLinkvertiseOptions(e.target.checked);
+                    }}
+                  />
+                )}
+              />
+            </div>
+
+            {showLinkvertiseOptions && (
+              <>
+                <div className={styles.input}>
+                  <BedrockText
+                    textAlign="start"
+                    text="Linkvertise User Id"
+                    type="p"
+                    color="white"
+                  />
+                  <Input
+                    placeholder="Linkvertise User Id"
+                    className={styles.input}
+                    {...register("linkvertiseId")}
+                  />
+
+                  {errors["linkvertiseId"] && (
+                    <BedrockText
+                      type="p2"
+                      extraClassName={styles.error}
+                      text={errors["linkvertiseId"]?.message as string}
+                      textAlign="start"
+                    />
+                  )}
+                </div>
+                <div className={styles.input}>
+                  <BedrockText
+                    textAlign="start"
+                    text="Linkvertise Anti-bypass Token"
+                    type="p"
+                    color="white"
+                  />
+                  <Input
+                    placeholder="Linkvertise Anti-bypass Token"
+                    className={styles.input}
+                    type="password"
+                    {...register("linkvertiseSecret")}
+                  />
+
+                  {errors["linkvertiseSecret"] && (
+                    <BedrockText
+                      type="p2"
+                      extraClassName={styles.error}
+                      text={errors["linkvertiseSecret"]?.message as string}
+                      textAlign="start"
+                    />
+                  )}
+                </div>
+              </>
+            )}
+          </Popup.Part>
+
+          <CardDivider />
+          <Popup.Part>
+            {admin && (
+              <Controller
+                name="banned"
+                control={control}
+                render={({ field }) => (
+                  <Button
+                    type={field.value ? "dark" : "red"}
+                    width="100%"
+                    center
+                    onClick={() => field.onChange(!field.value)}
+                    buttonType="submit"
+                  >
+                    <BedrockText type="p" text={field.value ? "Unban" : "Ban"} color="white" />
+                  </Button>
+                )}
+              />
+            )}
+            <Button type="green" buttonType="submit" center width="100%">
+              <BedrockText type="p" text="Save Settings" color="white" />
             </Button>
-          )}
-        </div>
-      </form>
+            {!admin && (
+              <Button type="dark" width="100%" center onClick={logout}>
+                <BedrockText type="p" text="Logout" color="white" />
+              </Button>
+            )}
+          </Popup.Part>
+        </form>
+      </Popup.Wrapper>
     </Popup>
   );
 };
