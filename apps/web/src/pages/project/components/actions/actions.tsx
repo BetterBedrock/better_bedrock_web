45e941b3c631c6e95223120f7e4c236be95d7d7b
iDarkQ <matikuki3@gmail.com>
2025-09-29 10:36:49 +0100

    style(actions.tsx): remove useless brackets

--- apps/web/src/pages/project/components/actions/actions.tsx
+++ apps/web/src/pages/project/components/actions/actions.tsx
@@ -50,7 +50,7 @@ export const Actions = () => {
 
       <PopupWrapper popup={(close) => <ActionsDeletePopup close={close} />}>
         <Button width="100%" height="100%" type="red" center>
-          <BedrockText text={"Deletion Options"} type="p" color="white" />
+          <BedrockText text="Deletion Options" type="p" color="white" />
         </Button>
       </PopupWrapper>
 
