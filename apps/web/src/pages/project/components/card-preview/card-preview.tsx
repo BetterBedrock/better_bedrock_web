a1e937f5307915f371307aae213a401ab79b0651
iDarkQ <matikuki3@gmail.com>
2025-09-29 10:37:44 +0100

    refactor(card-preview): replace managing state of popup with popupwrapper

--- apps/web/src/pages/project/components/card-preview/card-preview.tsx
+++ apps/web/src/pages/project/components/card-preview/card-preview.tsx
@@ -8,63 +8,63 @@ import { ButtonGroup } from "~/components/button-group/button-group";
 import { styles } from ".";
 import { useProjectManager } from "~/pages/project/providers/project-manager";
 import { Routes } from "~/utils/routes";
-import { useState } from "react";
 import { useProject } from "~/providers/project";
 import { CardPreviewDeclinePopup } from "~/pages/project/components/card-preview/card-preview-decline-popup";
 import { HeaderTitle } from "~/pages/project/components/header";
+import { PopupWrapper } from "~/components/bedrock/popup/popup-wrapper";
 
 export const CardPreview = () => {
   const navigate = useNavigate();
-  const [declineOpen, setDeclineOpen] = useState(false);
 
   const { decline, publish } = useProject();
   const { selectedProject } = useProjectManager();
   return (
-    <>
-      {declineOpen && (
-        <CardPreviewDeclinePopup
-          onCancel={() => setDeclineOpen(false)}
-          onSubmit={async (reason) => {
-            await decline(selectedProject!.id, selectedProject!.title, reason);
-            navigate(Routes.PANEL_PROJECTS);
-          }}
-        />
-      )}
-      <Card sub>
-        <div className={styles.editor}>
-          <HeaderTitle title="Review" />
-        </div>
-        <CardDivider sub />
-        <div className={styles.editor}>
-          {selectedProject && <GridDownloadCard project={{ ...selectedProject }} />}
-        </div>
-        <CardDivider sub />
-        <div className={styles.editor}>
-          <ButtonGroup>
-            <PopupConfirmation
-              description="Are you sure you want to publish this project?"
-              confirmText="Publish"
-              confirmType="gold"
+    <Card sub>
+      <div className={styles.editor}>
+        <HeaderTitle title="Review" />
+      </div>
+      <CardDivider sub />
+      <div className={styles.editor}>
+        {selectedProject && <GridDownloadCard project={{ ...selectedProject }} />}
+      </div>
+      <CardDivider sub />
+      <div className={styles.editor}>
+        <ButtonGroup>
+          <PopupConfirmation
+            description="Are you sure you want to publish this project?"
+            confirmText="Publish"
+            confirmType="gold"
+          >
+            <Button
+              width="100%"
+              height="100%"
+              type="gold"
+              center
+              onClick={async () => {
+                await publish(selectedProject!.id, selectedProject!.title);
+                navigate(Routes.PANEL_PROJECTS);
+              }}
             >
-              <Button
-                width="100%"
-                height="100%"
-                type="gold"
-                center
-                onClick={async () => {
-                  await publish(selectedProject!.id, selectedProject!.title);
+              <BedrockText text="Publish" type="p" color="white" />
+            </Button>
+          </PopupConfirmation>
+          <PopupWrapper
+            popup={(close) => (
+              <CardPreviewDeclinePopup
+                onCancel={close}
+                onSubmit={async (reason) => {
+                  await decline(selectedProject!.id, selectedProject!.title, reason);
                   navigate(Routes.PANEL_PROJECTS);
                 }}
-              >
-                <BedrockText text="Publish" type="p" color="white" />
-              </Button>
-            </PopupConfirmation>
-            <Button width="100%" type="red" onClick={() => setDeclineOpen(true)} center>
+              />
+            )}
+          >
+            <Button width="100%" type="red" center>
               <BedrockText text="Decline Project" type="p" color="white" />
             </Button>
-          </ButtonGroup>
-        </div>
-      </Card>
-    </>
+          </PopupWrapper>
+        </ButtonGroup>
+      </div>
+    </Card>
   );
 };
