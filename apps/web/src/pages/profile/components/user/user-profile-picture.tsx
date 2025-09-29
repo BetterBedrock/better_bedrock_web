59db145d5ac3c1c6dba933b1ed9d234268bc4005
iDarkQ <matikuki3@gmail.com>
2025-09-29 10:33:37 +0100

    refactor(user): utilize avatar.profile component to display profile pic

--- apps/web/src/pages/profile/components/user/user-profile-picture.tsx
+++ apps/web/src/pages/profile/components/user/user-profile-picture.tsx
@@ -1,9 +1,10 @@
 import { styles } from ".";
-import Steve from "~/assets/images/avatars/Steve.png";
-import Sumssatsuma from "~/assets/images/avatars/sumssatsuma.png";
+import { Avatar } from "~/components/avatar";
 
 interface UserProfilePicture {
-    special?: boolean;
+  name: string;
 }
 
-export const UserProfilePicture = ({special}: UserProfilePicture) => <img src={special ? Sumssatsuma : Steve} className={styles.image} />;
+export const UserProfilePicture = ({ name }: UserProfilePicture) => (
+  <Avatar.Profile className={styles.image} name={name} />
+);
