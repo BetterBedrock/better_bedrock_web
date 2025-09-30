3f0ccc235cf38b4562eb0d106f97ade6a2ab084d
iDarkQ <matikuki3@gmail.com>
2025-09-30 10:48:53 +0100

    refactor(url.tsx): create baseFrontendUrl

--- apps/web/src/types/react-app-env.d.ts
+++ apps/web/src/types/react-app-env.d.ts
@@ -12,7 +12,9 @@ interface ImportMetaEnv {
     readonly VITE_LINKVERTISE_ID: string;
     readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
     readonly VITE_LOCAL_BACKEND_URL: string;
+    readonly VITE_LOCAL_FRONTEND_URL: string;
     readonly VITE_BACKEND_URL: string;
+    readonly VITE_FRONTEND_URL: string;
     readonly VITE_GOOGLE_CLIENT_ID: string;
     readonly VITE_DEBUG: string;
 }
