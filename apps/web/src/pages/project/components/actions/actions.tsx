9eeed7831d7607e47d6abf7e48c49a10d6c48131
iDarkQ <matikuki3@gmail.com>
2025-10-05 13:05:53 +0100

    fix(web): buttons have different widths

--- apps/web/src/pages/project/components/actions/actions.tsx
+++ apps/web/src/pages/project/components/actions/actions.tsx
@@ -7,6 +7,7 @@ import { ActionsDeletePopup } from "~/pages/project/components/actions/actions-d
 import { ActionsSave } from "~/pages/project/components/actions/actions-save";
 import { useProjectManager } from "~/pages/project/providers/project-manager";
 import { useProject } from "~/providers/project";
+import { styles } from ".";
 
 export const Actions = () => {
   const { submitProject, cancelSubmission } = useProject();
@@ -36,6 +37,7 @@ export const Actions = () => {
         description="You are about to submit your project for verification process which will take up to 24h, if you are unsure, or want to make a change, you can alaways cancel the submission."
         confirmText="Submit"
         ignore={selectedProject?.submitted}
+        className={styles.button}
       >
         <Button width="100%" height="100%" type="white" onClick={handleSubmission} center>
           <BedrockText
@@ -46,7 +48,10 @@ export const Actions = () => {
         </Button>
       </PopupConfirmation>
 
-      <PopupWrapper popup={(close) => <ActionsDeletePopup close={close} />}>
+      <PopupWrapper
+        popup={(close) => <ActionsDeletePopup close={close} />}
+        className={styles.button}
+      >
         <Button width="100%" height="100%" type="red" center>
           <BedrockText text="Deletion Options" type="p" color="white" />
         </Button>
