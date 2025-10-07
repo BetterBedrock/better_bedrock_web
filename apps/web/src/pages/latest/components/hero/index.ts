4c6b39b5628a5ee4e03d710f193e17b93a7caa76
iDarkQ <matikuki3@gmail.com>
2025-10-07 10:23:15 +0100

    refactor(latest): remove latest page

--- apps/web/src/pages/latest/components/hero/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./hero";
-export * from "./hero-action";
-export * from "./hero-description";
-export * from "../gameplay/gameplay";
-export * from "../gameplay/gameplay-gallery"
-export * from "./hero-title";
-export { default as styles } from "./hero.module.scss";
