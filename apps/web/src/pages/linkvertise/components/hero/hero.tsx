70dc278caeddb8d4de0086b94cffbb8f212abc12
AxmBro <axmbro@gmail.com>
2025-10-08 20:32:48 +0200

    adjust faq

--- apps/web/src/pages/linkvertise/components/hero/hero.tsx
+++ apps/web/src/pages/linkvertise/components/hero/hero.tsx
@@ -237,7 +237,11 @@ const questions: InformationFAQQuestion[] = [
   },
   {
     question: "What do I need to do to receive money?",
-    answer: "To receive money you need to set up payout settings on linkvertise website; Local Bank Transfer / Sepa Transfer, Wire Transfer, PayPal. On our side we require you to set up User ID and Anti-bypass Token in account settings. Everything goes directly to your account without any operations here. ",
+    answer: "To receive money you need to set up payout settings on linkvertise website; Local Bank Transfer / Sepa Transfer, Wire Transfer, PayPal. On our side we require you to set up User ID and Anti-bypass Token in account settings. To set up this entire functionality, follow tutorial above or text instructions below. Everything goes directly to your account without any operations here.",
+  },
+  {
+    question: "How to earn more money? (trick)",
+    answer: "Whenever you want to payout your earnings, make sure to do it on wallet with euro currency. This way you will avoid 1.9% to 3% FX fee ;)",
   },
 ];
 
