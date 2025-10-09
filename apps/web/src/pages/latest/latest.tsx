75f90fa3231e6379f4cf4541c49ced8d3264fcef
iDarkQ <matikuki3@gmail.com>
2025-10-09 08:22:19 +0100

    fix(latest.tsx): fix link to the latest page

--- apps/web/src/pages/latest/latest.tsx
+++ apps/web/src/pages/latest/latest.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
 
 export const Latest = () => {
-  return <Navigate to="/project/preview/better_bedrock" replace />;
+  return <Navigate to="/project/preview/better_bedrock_texture_pack" replace />;
 };
