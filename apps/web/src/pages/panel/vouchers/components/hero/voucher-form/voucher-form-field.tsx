a9f0d28ff907385b231567968e47279f56a72a43
iDarkQ <matikuki3@gmail.com>
2025-10-24 17:27:12 +0100

    refactor(panel/vouchers): implement SRP

--- apps/web/src/pages/panel/vouchers/components/hero/voucher-form/voucher-form-field.tsx
+++ apps/web/src/pages/panel/vouchers/components/hero/voucher-form/voucher-form-field.tsx
@@ -1,9 +1,8 @@
-import { Control, Controller, FieldErrors } from "react-hook-form";
+import { Control, FieldErrors } from "react-hook-form";
 import { BedrockText } from "~/components/bedrock/bedrock-text";
-import { Input } from "~/components/bedrock/input";
-import { InputSwitch } from "~/components/bedrock/input/input-switch";
-import { styles } from ".";
+import { styles, VoucherFormError } from ".";
 import { VoucherDto } from "~/lib/api";
+import { VoucherFormFieldController } from "~/pages/panel/vouchers/components/hero/voucher-form/voucher-form-field-controller";
 
 interface VoucherFormFieldProps {
   name: keyof VoucherDto;
@@ -25,54 +24,13 @@ export const VoucherFormField = ({
 }: VoucherFormFieldProps) => (
   <div className={styles.field} key={name}>
     <BedrockText textAlign="start" text={label} type="p" color="white" />
-    <Controller
+    <VoucherFormFieldController
       name={name}
+      type={type}
       control={control}
-      render={({ field }) => {
-        if (
-          type === "checkbox" &&
-          (typeof field.value === "undefined" || typeof field.value === "boolean")
-        ) {
-          return (
-            <InputSwitch
-              type="checkbox"
-              className={styles.input}
-              placeholder={placeholder}
-              checked={!!field.value}
-              onChange={(e) => field.onChange(e.target.checked)}
-              onBlur={field.onBlur}
-            />
-          );
-        }
-
-        if (
-          typeof field.value === "string" ||
-          typeof field.value === "undefined" ||
-          typeof field.value === "number"
-        ) {
-          return (
-            <Input
-              type={type}
-              placeholder={placeholder}
-              className={styles.input}
-              value={field.value ?? ""}
-              onChange={field.onChange}
-              onBlur={field.onBlur}
-            />
-          );
-        }
-
-        return <></>;
-      }}
+      placeholder={placeholder}
     />
 
-    {errors[name] && (
-      <BedrockText
-        type="p2"
-        extraClassName={styles.error}
-        text={errors[name]?.message as string}
-        textAlign="start"
-      />
-    )}
+    {errors[name] && <VoucherFormError message={errors[name]?.message as string} />}
   </div>
 );
