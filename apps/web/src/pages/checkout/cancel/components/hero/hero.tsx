89f6193db25b70cdd00c88195cac9359003672d6
iDarkQ <matikuki3@gmail.com>
2025-10-24 17:26:08 +0100

    refactor(checkout): implement SRP

--- apps/web/src/pages/checkout/cancel/components/hero/hero.tsx
+++ apps/web/src/pages/checkout/cancel/components/hero/hero.tsx
@@ -1,32 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { BedrockText } from "~/components/bedrock/bedrock-text";
-import { Button } from "~/components/bedrock/button";
-import { Routes } from "~/utils/routes";
-import { styles } from ".";
+import { HeroAction, HeroDescription, HeroTitle } from ".";
 
-export const Hero = () => {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <BedrockText type="h1" text="CHECKOUT CANCELLED" color="white" font="Minecraft" />
-      <BedrockText
-        type="p"
-        color="white"
-        text={
-          "You have cancelled your checkout. If you have any questions or need assistance, please reach out to us."
-        }
-      />
-      <Button
-        width="100%"
-        type="green"
-        onClick={() => {
-          navigate(Routes.HOME);
-        }}
-        className={styles.return}
-        center
-      >
-        <BedrockText text="Return to Home" type="p" color="white" />
-      </Button>
-    </div>
-  );
-};
+export const Hero = () => <div>
+  <HeroTitle />
+  <HeroDescription />
+  <HeroAction />
+</div>;
--- apps/web/src/pages/checkout/success/components/hero/hero.tsx
+++ apps/web/src/pages/checkout/success/components/hero/hero.tsx
@@ -1,53 +1,15 @@
-import { useEffect } from "react";
-import { useCookies } from "react-cookie";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { BedrockText } from "~/components/bedrock/bedrock-text";
-import { Button } from "~/components/bedrock/button";
-import { useCheckout } from "~/providers/checkout";
-import { Routes } from "~/utils/routes";
-import { styles } from ".";
+import { useSearchParams } from "react-router-dom";
+import { HeroAction, HeroDescription, HeroTitle, useActiveVoucher } from ".";
 
 export const Hero = () => {
-  const navigate = useNavigate();
-  const [__, setCookie] = useCookies(["voucher"]);
   const [searchParams, _] = useSearchParams();
-  const { activateVoucher } = useCheckout();
-
-  useEffect(() => {
-    activateVoucher(searchParams.get("checkoutId") || "").then((voucher) => {
-      if (voucher === null) {
-        return;
-      }
-
-      setCookie("voucher", voucher);
-    });
-  }, []);
+  useActiveVoucher({ checkoutId: searchParams.get("checkoutId") || "" });
 
   return (
     <div>
-      <BedrockText
-        type="h1"
-        text="Payment Finished Successfuly"
-        color="white"
-        font="Minecraft"
-      />
-      <BedrockText
-        type="p"
-        color="white"
-        text={
-          "You successfully completed your payment. Thank you for your support! If you have any questions or need assistance, please reach out to us."
-        }
-      />
-      <Button
-        width="100%"
-        height="auto"
-        className={styles.return}
-        type="green"
-        onClick={() => navigate(Routes.HOME)}
-        center
-      >
-        <BedrockText text="Return to Home" type="p" color="white" />
-      </Button>
+      <HeroTitle />
+      <HeroDescription />
+      <HeroAction />
     </div>
   );
 };
