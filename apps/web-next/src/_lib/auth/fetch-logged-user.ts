f76afc565d8c1585a0701ec53f5b9a30283a4791
iDarkQ <matikuki3@gmail.com>
2025-10-31 20:49:43 +0000

    feat(_lib/auth): create server functions

---
+++ apps/web-next/src/_lib/auth/fetch-logged-user.ts
@@ -0,0 +1,11 @@
+import { authenticateRequest } from "@/_services";
+import { cookies } from "next/headers";
+
+export const fetchLoggedUser = async () => {
+    const cookieStore = await cookies();
+    const secret = cookieStore.get("secret")?.value;
+
+    const user = secret ? (await authenticateRequest(secret)).data : undefined;
+
+    return user;
+}
