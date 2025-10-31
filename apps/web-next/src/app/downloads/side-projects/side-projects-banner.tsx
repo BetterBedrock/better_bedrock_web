dfc21018b3a126d1ef42bf6638006748498ac8bb
iDarkQ <matikuki3@gmail.com>
2025-10-31 20:57:33 +0000

    feat(downloads/side-projects): create side-projects page

---
+++ apps/web-next/src/app/downloads/side-projects/side-projects-banner.tsx
@@ -0,0 +1,8 @@
+import { Banner } from "~/components/bedrock/banner";
+
+export const SideProjectsBanner = () => (
+  <Banner
+    type="info"
+    message="Better Bedrock content is not yet prepared, please wait a while and refresh this site."
+  />
+);
