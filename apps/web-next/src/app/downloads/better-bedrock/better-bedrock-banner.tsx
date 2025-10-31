a719ab8a354046a78504028e7e7c9912ec790505
iDarkQ <matikuki3@gmail.com>
2025-10-31 20:56:55 +0000

    feat(downloads/better-bedrock): create downloads better bedrock page

---
+++ apps/web-next/src/app/downloads/better-bedrock/better-bedrock-banner.tsx
@@ -0,0 +1,8 @@
+import { Banner } from "@/_components/banner";
+
+export const BetterBedrockBanner = () => (
+  <Banner
+    type="info"
+    message="Better Bedrock content is not yet prepared, please wait a while and refresh this site."
+  />
+);
