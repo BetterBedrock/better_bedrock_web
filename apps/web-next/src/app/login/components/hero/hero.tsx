979c5bc007a959f45627d42b6b4eb26443156e42
iDarkQ <matikuki3@gmail.com>
2025-11-01 20:59:18 +0000

    feat(login): convert login page form web to web-next

---
+++ apps/web-next/src/app/login/components/hero/hero.tsx
@@ -0,0 +1,9 @@
+import { HeroHeader, HeroActions } from ".";
+import { styles } from ".";
+
+export const Hero = () => (
+  <div className={styles.wrapper}>
+    <HeroHeader />
+    <HeroActions />
+  </div>
+);
