b964dc7782aa86d63d24555264c8565255a6696f
iDarkQ <matikuki3@gmail.com>
2025-11-05 21:39:51 +0000

    refactor(checkout.tsx): strip checkout of useless functions

--- apps/web-next/src/_providers/checkout.tsx
+++ apps/web-next/src/_providers/checkout.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import {
-  VoucherDto,
   CheckoutOffersDto,
-  Configuration,
-  CheckoutApi,
 } from "@/_lib/api";
-import { useNotification } from "@/_providers/notification";
-import { baseUrl } from "@/utils/url";
+import { fetchCheckoutOffers } from "@/_lib/checkout";
 import {
   createContext,
   ReactNode,
@@ -17,13 +13,6 @@ import {
 } from "react";
 
 interface CheckoutContextProps {
-  createSession: (
-    priceId: string
-  ) => Promise<{ checkoutId: string } | undefined>;
-  activateVoucher: (
-    checkoutId?: string,
-    code?: string
-  ) => Promise<VoucherDto | undefined>;
   fetchCheckoutOffers: () => void;
   offers: CheckoutOffersDto | undefined;
 }
@@ -40,57 +29,19 @@ export const CheckoutProvider = ({ children }: CheckoutProviderProps) => {
   const [offers, setOffers] = useState<CheckoutOffersDto | undefined>(
     undefined
   );
-  const { throwError } = useNotification();
-
-  const config = new Configuration({
-    basePath: baseUrl,
-  });
-
-  const checkoutApi = new CheckoutApi(config);
-
-  const fetchCheckoutOffers = async () => {
-    try {
-      const { data } = await checkoutApi.checkoutControllerOffers();
-
-      setOffers(data);
-    } catch (err) {
-      throwError(err, "Failed to fetch offers");
-    }
-  };
-
-  const createSession = async (priceId: string) => {
-    try {
-      const { data } = await checkoutApi.checkoutControllerCreate(priceId);
-      return data;
-    } catch (err) {
-      throwError(err, "Failed to create checkout session");
-    }
-  };
-
-  const activateVoucher = async (checkoutId?: string, code?: string) => {
-    try {
-      const { data } = await checkoutApi.checkoutControllerActivate(
-        checkoutId,
-        code
-      );
-
-      return data;
-    } catch (err) {
-      throwError(err, "Failed to activate voucher");
-    }
+  const fetchOffers = async () => {
+    setOffers(await fetchCheckoutOffers());
   };
 
   useEffect(() => {
     // Leave this as the only case where we pre-fetch something from the server on web load
     // It is left like this to make sure visitors count updates
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    fetchCheckoutOffers();
+    fetchOffers();
   }, []);
 
   return (
-    <CheckoutContext.Provider
-      value={{ createSession, activateVoucher, fetchCheckoutOffers, offers }}
-    >
+    <CheckoutContext.Provider value={{ fetchCheckoutOffers, offers }}>
       {children}
     </CheckoutContext.Provider>
   );
