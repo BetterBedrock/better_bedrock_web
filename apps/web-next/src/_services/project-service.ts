182966bbb1150910b9476f49be74c9d19a9ab4ce
iDarkQ <matikuki3@gmail.com>
2025-11-06 06:49:23 +0000

    feat(project-service.ts): create more requests (CRUD for comments) & delete and post for rating & fetch basic and detailed project

--- apps/web-next/src/_services/project-service.ts
+++ apps/web-next/src/_services/project-service.ts
@@ -18,3 +18,21 @@ export const searchProjectsRequest = async (type?: ProjectType, order?: SearchOr
     text,
     page
 );
+
+export const fetchProjectsBasicInfoRequest = async (ids: string[]) => await projectApi.projectControllerProjectsBasicInfo({
+    ids,
+});
+
+export const fetchProjectDetailsRequest = async (id: string) => projectApi.projectControllerProjectDetails(id);
+
+export const deleteRatingRequest = async (id: string, secret: string) => await projectApi.projectControllerDeleteRating(id, { headers: { "Authorization": `Bearer ${secret}` } });
+export const postRatingRequest = async (id: string, rating: number, secret: string) => await projectApi.projectControllerRateProject(
+    id,
+    rating,
+    { headers: { Authorization: `Bearer ${secret}` } }
+);
+
+export const fetchCommentsRequest = async (id: string) => projectApi.projectControllerComments(id);
+export const deleteCommentRequest = async (id: string, secret: string) => projectApi.projectControllerDeleteComment(id, { headers: { Authorization: `Bearer ${secret}` } });
+export const postCommentRequest = async (id: string, content: string, secret: string) => projectApi.projectControllerPostComment(id, { content }, { headers: { Authorization: `Bearer ${secret}` } });
+export const replyToCommentRequest = async (id: string, parentId: string, content: string, secret: string) => projectApi.projectControllerReplyToComment(id, parentId, { content }, { headers: { Authorization: `Bearer ${secret}` } });
