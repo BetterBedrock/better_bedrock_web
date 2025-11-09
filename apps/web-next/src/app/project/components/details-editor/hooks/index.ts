b9e293a3ee78babd7597e77d2bf30805b891e3ff
iDarkQ <matikuki3@gmail.com>
2025-11-09 07:06:13 +0000

    feat(project/details-editor): copy details-editor component from react app

---
+++ apps/web-next/src/app/project/components/details-editor/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from "./use-details-editor-better-bedrock-content";
+export * from "./use-details-editor-download-file";
+export * from "./use-details-editor-project-type";
+export * from "./use-details-editor-tags";
