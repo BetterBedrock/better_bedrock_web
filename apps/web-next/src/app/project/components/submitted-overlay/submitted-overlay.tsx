d4de572465fae481eef5af4185a547d26364f832
iDarkQ <matikuki3@gmail.com>
2025-11-09 07:07:24 +0000

    feat(project/submitted-overlay): copy submitted-overlay component from react app

---
+++ apps/web-next/src/app/project/components/submitted-overlay/submitted-overlay.tsx
@@ -0,0 +1,13 @@
+import { BedrockText } from "@/_components/bedrock-text";
+import { styles } from ".";
+
+export const SubmittedOverlay = () => (
+  <div className={styles.overlay}>
+    <BedrockText type="h2" text="The project has been submitted" color="white" font="Minecraft" />
+    <BedrockText
+      type="p"
+      text="Any edits are no longer saved. To edit content, cancel submission"
+      color="white"
+    />
+  </div>
+);
