c51812ed0cbd72c64ccba5eee5c977cd431924c5
iDarkQ <matikuki3@gmail.com>
2025-11-11 10:55:03 +0000

    feat(analytics-service.ts): create fetchAllAnalytics request

--- apps/web-next/src/_services/analytics-service.ts
+++ apps/web-next/src/_services/analytics-service.ts
@@ -1,14 +1,18 @@
 import { AnalyticsApi, Configuration } from "@/_lib/api";
+import { axiosCustomInstance } from "@/_lib/client";
 import { baseUrl } from "@/utils/url";
 
 const config = new Configuration({
     basePath: baseUrl,
 });
 
-const analyticsApi = new AnalyticsApi(config);
+const analyticsApi = new AnalyticsApi(config, undefined, axiosCustomInstance);
+
+export const fetchAllAnalyticsRequest = async (secret: string) =>
+    analyticsApi.analyticsControllerAnalytics({ headers: { "Authorization": `Bearer ${secret}` } });
 
 export const fetchUserAnalyticsRequest = async (
     id: string,
     secret: string,
 ) =>
-    await analyticsApi.analyticsControllerUser(id, { headers: { "Authorization": `Bearer ${secret}` } });
+    analyticsApi.analyticsControllerUser(id, { headers: { "Authorization": `Bearer ${secret}` } });
