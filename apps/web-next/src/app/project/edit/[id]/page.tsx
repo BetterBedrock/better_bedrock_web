906bf164a7f06f04f4db21279c62e0f21135a633
iDarkQ <matikuki3@gmail.com>
2025-11-12 11:01:16 +0000

    refactor(edit/[id]/page.tsx): simplify check for whether user has permissions

--- apps/web-next/src/app/project/edit/[id]/page.tsx
+++ apps/web-next/src/app/project/edit/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { fetchLoggedUser } from "@/_lib/auth";
+import { fetchDraftsDetails } from "@/_lib/projects/fetch-draft-details";
 import { fetchProjectDetails } from "@/_lib/projects/fetch-project-details";
 import { DownloadProvider } from "@/_providers/download";
 import { ProjectPageProps } from "@/app/project";
 import { Header, Description, Actions } from "@/app/project/components";
+import { DetailsEditor } from "@/app/project/components/details-editor";
 import { DownloadButton } from "@/app/project/components/download-button";
 import { Thumbnail } from "@/app/project/components/thumbnail";
 import { ProjectManagerProvider } from "@/app/project/providers/project-manager";
@@ -11,26 +13,23 @@ import { notFound } from "next/navigation";
 export default async function Edit({ params }: ProjectPageProps) {
   const loadedParams = await params;
   const user = await fetchLoggedUser();
-  const project = await fetchProjectDetails(loadedParams.id);
+  const project = await fetchDraftsDetails(loadedParams.id);
 
-  const hasAccess = user?.admin || user?.id === project?.userId;
-
-  if (!hasAccess || !project) {
+  if (!project || !user) {
     notFound();
   }
 
   return (
     <>
       <Header mode="edit" selectedProject={project} />
-
-      {/* <DetailsEditor /> */}
+      <DownloadButton detailedProject={project} user={user} />
 
       <ProjectManagerProvider detailedProject={project}>
-        <DownloadButton detailedProject={project} user={user}/>
+        <DetailsEditor detailedProject={project} user={user} />
         <Thumbnail detailedProject={project} />
         <Description mode="edit" detailedProject={project} />
+        <Actions detailedProject={project} />
       </ProjectManagerProvider>
-      {/* <Actions /> */}
     </>
   );
 }
