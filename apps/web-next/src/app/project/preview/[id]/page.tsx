c5e28ead69a3435f424a9492b79bbe0212756131
iDarkQ <matikuki3@gmail.com>
2025-12-14 16:28:24 +0000

    feat(page.tsx): generate title and description to the project preview page

--- apps/web-next/src/app/project/preview/[id]/page.tsx
+++ apps/web-next/src/app/project/preview/[id]/page.tsx
@@ -1,13 +1,31 @@
 import { fetchLoggedUser } from "@/_lib/auth";
 import { fetchProjectDetails } from "@/_lib/projects/fetch-project-details";
-import { DownloadProvider } from "@/_providers/download";
-import { Description } from "@/app/project/components";
 import { Comments } from "@/app/project/components/comments";
+import { Description } from "@/app/project/components/description";
 import { DownloadButton } from "@/app/project/components/download-button";
 import { Header } from "@/app/project/components/header";
 import { RateProject } from "@/app/project/components/rate-project";
 import { ProjectPageProps } from "@/app/project/layout";
 import { notFound } from "next/navigation";
+
+export const generateMetadata = async ({ params }: ProjectPageProps) => {
+  const loadedParams = await params;
+  const project = await fetchProjectDetails(loadedParams.id);
+
+  if (!project) return notFound();
+
+  return {
+    title: project.title,
+    description:
+      project.description ??
+      "The best texture packs, scripts, maps, skins, and more for Minecraft PE on Better Bedrock.",
+    openGraph: {
+      title: project.title,
+      description: project.description,
+      images: project.thumbnail,
+    },
+  };
+}
 
 export default async function Preview({ params }: ProjectPageProps) {
   const loadedParams = await params;
