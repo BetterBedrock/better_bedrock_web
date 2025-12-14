e1db7b4ae8ce59bce3f6119a3a7df2b3e31eb5fb
iDarkQ <matikuki3@gmail.com>
2025-12-14 16:26:14 +0000

    feat(page.tsx): add title and description to the terms page

--- apps/web-next/src/app/terms/page.tsx
+++ apps/web-next/src/app/terms/page.tsx
@@ -3,6 +3,12 @@ import { Section } from "@/_components/section";
 
 import { styles } from ".";
 
+export const metadata = {
+  title: "Terms of Service",
+  description:
+    "Read the Terms of Service for Better Bedrock, outlining the rules and guidelines for using our platform to access Minecraft PE texture packs, scripts, maps, skins, and more.",
+};
+
 export default function Terms() {
   return (
     <Section
