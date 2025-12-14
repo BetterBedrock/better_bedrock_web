c99bb1f0d739e471de236fc30c4b569ac44f90ee
iDarkQ <matikuki3@gmail.com>
2025-12-14 16:26:05 +0000

    feat(page.tsx): add title and description to the verify page

--- apps/web-next/src/app/verify/page.tsx
+++ apps/web-next/src/app/verify/page.tsx
@@ -8,6 +8,12 @@ interface FetchProps {
   }>;
 }
 
+export const metadata = {
+  title: "Download Content Verification",
+  description:
+    "Verify your session and begin downloading the best Minecraft PE texture packs, scripts, maps, skins, and more from Better Bedrock securely.",
+};
+
 export default async function Verify({ searchParams }: FetchProps) {
   const hash = (await searchParams).hash;
 
