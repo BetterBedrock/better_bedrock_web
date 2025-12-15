643fc1d111370ae96003e0b6c7fab8ca6e55b2fe
iDarkQ <matikuki3@gmail.com>
2025-12-15 17:26:06 +0000

    refactor(index.ts): export newly created components

--- apps/web-next/src/app/project/components/submitted-overlay/index.ts
+++ apps/web-next/src/app/project/components/submitted-overlay/index.ts
@@ -1,2 +1,4 @@
 export * from "./submitted-overlay";
+export * from "./submitted-overlay-title";
+export * from "./submitted-overlay-description";
 export { default as styles } from "./submitted-overlay.module.scss";
