2d0ab05f5d356a6d4a430eeccc6835701b7b8d70
iDarkQ <matikuki3@gmail.com>
2025-12-18 12:34:11 +0000

    refactor(trailer-video.tsx): utilize youtube video component

--- apps/web-next/src/app/components/hero/trailer/trailer-video.tsx
+++ apps/web-next/src/app/components/hero/trailer/trailer-video.tsx
@@ -1,16 +1,8 @@
-import { styles } from ".";
+import { YoutubeVideo } from "@/_components/youtube-video";
 
 export const TrailerVideo = () => (
-  <div className={styles.video}>
-    <iframe
-      width="100%"
-      height="100%"
-      src="https://www.youtube.com/embed/uCB4S8EE28k"
-      title="Better Bedrock: Projects | Release Trailer"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
-  </div>
+  <YoutubeVideo
+    title="Better Bedrock: Projects | Release Trailer"
+    id="uCB4S8EE28k"
+  />
 );
