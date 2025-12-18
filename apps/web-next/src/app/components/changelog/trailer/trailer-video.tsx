e62058921f44f0b5c471fe8fe3ae2c7a805200f9
iDarkQ <matikuki3@gmail.com>
2025-12-18 12:33:21 +0000

    refactor(trailer-video.tsx): utilize youtube video component

--- apps/web-next/src/app/components/changelog/trailer/trailer-video.tsx
+++ apps/web-next/src/app/components/changelog/trailer/trailer-video.tsx
@@ -1,16 +1,8 @@
-import { styles } from ".";
+import { YoutubeVideo } from "@/_components/youtube-video/youtube-video";
 
 export const TrailerVideo = () => (
-  <div className={styles.video}>
-    <iframe
-      width="100%"
-      height="100%"
-      src="https://youtube.com/embed/4XSc0J0mrlU"
-      title="BETTER BEDROCK V8 RELEASE! The Best Utility Texture Pack for Minecraft Bedrock | Showcase Trailer"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
-  </div>
+  <YoutubeVideo
+    id="4XSc0J0mrlU"
+    title="BETTER BEDROCK V8 RELEASE! The Best Utility Texture Pack for Minecraft Bedrock | Showcase Trailer"
+  />
 );
