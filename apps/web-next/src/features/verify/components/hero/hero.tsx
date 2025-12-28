753401fd1af7d6cdf9b4a8ab5b6bb3c8d8a9a6dd
iDarkQ <matikuki3@gmail.com>
2025-12-28 22:45:42 +0000

    refactor(hero.tsx): change the way we handle voucher (use just code now)

--- apps/web-next/src/features/verify/components/hero/hero.tsx
+++ apps/web-next/src/features/verify/components/hero/hero.tsx
@@ -1,14 +1,13 @@
-"use server";
-
 import { verifyDownload } from "@/lib/downloads/verify-download";
 import { cookies } from "next/headers";
-import { VoucherDto } from "@/lib/api";
 import { DownloadProvider } from "@/providers/download";
 import { HeroHeader } from "./hero-header";
 import { HeroDownloadProgress } from "./hero-download-progress";
 import { HeroRedownloadMessage } from "./hero-redownload-message";
 
 import styles from "./hero.module.scss";
+import { redirect } from "next/navigation";
+import { Routes } from "@/utils/routes";
 
 interface HeroProps {
   hash?: string;
@@ -20,8 +19,12 @@ export const Hero = async ({ hash }: HeroProps) => {
 
   const downloadItem = await verifyDownload(
     hash ?? undefined,
-    (JSON.parse(voucher ?? "{}") as VoucherDto)?.code
+    voucher
   );
+
+  if (!downloadItem) {
+    redirect(Routes.DOWNLOADS_MAIN);
+  }
 
   return (
     <div className={styles.hero}>
