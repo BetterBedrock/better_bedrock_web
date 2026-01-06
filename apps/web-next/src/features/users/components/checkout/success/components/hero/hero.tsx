c4db04e18a4a25f6927a0809d497020b84b877b2
iDarkQ <matikuki3@gmail.com>
2026-01-06 22:58:02 +0000

    refactor(hero.tsx): remove handling cookies client-side

--- apps/web-next/src/features/users/components/checkout/success/components/hero/hero.tsx
+++ apps/web-next/src/features/users/components/checkout/success/components/hero/hero.tsx
@@ -2,20 +2,16 @@ import { SuccessPageProps } from "@/app/(users)/checkout/success/page";
 import { HeroAction } from "./hero-action";
 import { HeroDescription } from "./hero-description";
 import { HeroTitle } from "./hero-title";
-import { activateVoucher } from "@/lib/checkout/active-voucher";
+import { activateVoucher } from "@/features/project/hooks/use-active-voucher";
 import { redirect } from "next/navigation";
 import { Routes } from "@/utils/routes";
-import { cookies } from "next/headers";
 
 export const Hero = async ({ searchParams }: SuccessPageProps) => {
-  const cookie = await cookies();
   const fetchedParams = await searchParams;
   const voucher = await activateVoucher(fetchedParams.checkoutId);
   if (!voucher) {
     redirect(Routes.CHECKOUT_CANCEL);
   }
-
-  cookie.set("voucher", JSON.stringify(voucher), { path: "/" });
 
   return (
     <div>
