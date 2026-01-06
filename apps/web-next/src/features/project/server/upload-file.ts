ade80786c136a646e305522d725fe4ca792228a3
iDarkQ <matikuki3@gmail.com>
2026-01-06 22:58:14 +0000

    feat(upload-file.ts): create server action

---
+++ apps/web-next/src/features/project/server/upload-file.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { fetchSecret } from "@/lib/user";
+import { uploadFileRequest } from "@/services/project-service";
+
+export const uploadFile = async (id: string, file: File | undefined) => {
+    const secret = await fetchSecret();
+
+    const { data, error } = await uploadFileRequest(id, file, secret);
+
+    return { data, error };
+}
