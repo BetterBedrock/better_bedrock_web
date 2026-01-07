e1d5783cda52fe9817efeb27db1c006eed52c816
iDarkQ <matikuki3@gmail.com>
2026-01-07 23:01:11 +0000

    style(page.tsx): fix spacings

--- apps/web-next/src/app/(projects)/project/review/[id]/page.tsx
+++ apps/web-next/src/app/(projects)/project/review/[id]/page.tsx
@@ -2,13 +2,13 @@ import { fetchLoggedUser } from "@/lib/auth";
 import { fetchDraftsDetails } from "@/features/project/server/fetch-draft-details";
 import { CardPreview } from "@/features/project/components/card-preview/card-preview";
 import { Description } from "@/features/project/components/description/description";
-import { DownloadButton } from "@/features/project/components/download-button/download-button";
 import { Header } from "@/features/project/components/header/header";
 import { notFound } from "next/navigation";
 import {
   ProjectManagerProvider,
   ProjectPageProps,
 } from "@/features/project/providers/project-manager";
+import { Download } from "@/features/project/components/download/download";
 
 export const metadata = {
   title: "Review Submitted Project",
@@ -28,7 +28,7 @@ export default async function Review({ params }: ProjectPageProps) {
     <>
       <Header mode="view" selectedProject={project} />
       <Description mode="view" detailedProject={project} />
-      <DownloadButton detailedProject={project} />
+      <Download detailedProject={project} />
       <ProjectManagerProvider detailedProject={project}>
         <CardPreview />
       </ProjectManagerProvider>
