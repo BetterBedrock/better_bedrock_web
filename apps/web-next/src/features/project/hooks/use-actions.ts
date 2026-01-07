823ebbc7dd54a1df8165e971fef9a4056c754ffd
iDarkQ <matikuki3@gmail.com>
2026-01-07 23:06:25 +0000

    refactor(use-actions.ts): replace providers with server actions & handle success & error messages

--- apps/web-next/src/features/project/hooks/use-actions.ts
+++ apps/web-next/src/features/project/hooks/use-actions.ts
@@ -1,10 +1,14 @@
 "use client";
 
-import { useProject } from "@/providers/project";
 import { useProjectManager } from "@/features/project/providers/project-manager";
+import { cancelProject } from "@/features/project/server/cancel-project";
+import { submitProject } from "@/features/project/server/submit-project";
+import { useNotification } from "@/providers/notification";
+import { useRouter } from "next/navigation";
 
 export const useActions = () => {
-    const { submitProject, cancelSubmission } = useProject();
+    const router = useRouter();
+    const { sendNotification, throwError } = useNotification();
     const { handleSaveProject, selectedProject, setSelectedProject } = useProjectManager();
 
     const handleSubmission = async () => {
@@ -12,15 +16,37 @@ export const useActions = () => {
 
         if (!selectedProject.submitted) {
             await handleSaveProject(selectedProject);
-            const submission = await submitProject(selectedProject.id, selectedProject.title);
+            const { error } = await submitProject(selectedProject.id);
+            if (error) {
+                throwError(null, error);
+                return;
+            }
 
-            if (!submission) return;
+            sendNotification({
+                type: "info",
+                title: selectedProject.title,
+                label: "Project has been submitted for review",
+            });
+
             setSelectedProject((prev) => ({ ...prev!, submitted: true, error: null }));
             window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
         } else {
-            await cancelSubmission(selectedProject.id, selectedProject.title);
+            const { error } = await cancelProject(selectedProject.id);
+            if (error) {
+                throwError(null, error);
+                return;
+            }
+
+            sendNotification({
+                type: "info",
+                title: selectedProject.title,
+                label: "Project has been submitted for review",
+            });
+
             setSelectedProject((prev) => ({ ...prev!, submitted: false }));
         }
+
+        router.refresh();
     };
 
     return { handleSubmission, selectedProject, handleSaveProject };
