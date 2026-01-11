8fbb47ccff43c264a56602e39396b5064902f48f
AxmBro <axmbro@gmail.com>
2026-01-11 02:43:14 +0100

    feat: refactor ProjectsCard and FeaturedCard components for improved layout and styling; enhance ProjectsCardBanner with tutorial link

--- apps/web-next/src/features/project/components/featured-card/featured-card.tsx
+++ apps/web-next/src/features/project/components/featured-card/featured-card.tsx
@@ -2,14 +2,12 @@ import { Card, CardBody, CardDivider } from "@/components/card";
 import { FeaturedCardAds } from "@/features/project/components/featured-card/featured-card-ads";
 import { FeaturedCardTitle } from "@/features/project/components/featured-card/featured-card-title";
 
-import styles from "./featured-card.module.scss";
-
 export const FeaturedCard = () => (
-  <Card sub className={styles.main}>
+  <Card fullWidth>
     <CardBody>
       <FeaturedCardTitle />
     </CardBody>
-    <CardDivider sub />
+    <CardDivider />
     <CardBody>
       <FeaturedCardAds />
     </CardBody>
--- apps/web-next/src/features/project/components/projects-card/projects-card-search-bar.tsx
+++ apps/web-next/src/features/project/components/projects-card/projects-card-search-bar.tsx
@@ -4,16 +4,20 @@ import { Input } from "@/components/input";
 import { useProjectsCardSearch } from "../../providers/projects-card-search";
 
 import styles from "./projects-card.module.scss";
+import { ProjectsCardOrder } from "./projects-card-order";
 
 export const ProjectsCardSearchBar = () => {
   const { inputRef } = useProjectsCardSearch();
 
   return (
-    <Input
+    <div className={styles.searchbarContainer}>
+      <Input
         sub
-      ref={inputRef}
-      placeholder="Search for a project"
-      className={styles.searchbar}
-    />
+        ref={inputRef}
+        placeholder="Search for a project"
+        className={styles.searchbar}
+      />
+      <ProjectsCardOrder className={styles.orderHalf} />
+    </div>
   );
 };
