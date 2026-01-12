1dc3d2ae4a2aa9735cbcbff396a69d5ffeee60dc
AxmBro <axmbro@gmail.com>
2026-01-13 00:05:26 +0100

    feat: add videoContainer class for consistent margin in trailer components

--- apps/web-next/src/features/home/components/changelog/trailer/trailer-video.tsx
+++ apps/web-next/src/features/home/components/changelog/trailer/trailer-video.tsx
@@ -1,7 +1,9 @@
 import { YoutubeVideo } from "@/components/youtube-video/youtube-video";
+import styles from "../changelog.module.scss";
 
 export const TrailerVideo = () => (
   <YoutubeVideo
+    className={styles.videoContainer}
     id="4XSc0J0mrlU"
     title="BETTER BEDROCK V8 RELEASE! The Best Utility Texture Pack for Minecraft Bedrock | Showcase Trailer"
   />
