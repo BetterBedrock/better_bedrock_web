f634d6f5e4a7cadc1d660fdd0cc55c641e1f3637
AxmBro <axmbro@gmail.com>
2026-01-13 00:08:43 +0100

    fix: refactor Hero and ProjectsList components to use Card layout for improved structure and readability

--- apps/web-next/src/features/panel/projects/components/hero/hero.tsx
+++ apps/web-next/src/features/panel/projects/components/hero/hero.tsx
@@ -1,10 +1,9 @@
 import { Header } from "./header/header";
 import { ProjectsList } from "./projects-list/projects-list";
-import styles from "./hero.module.scss";
 
 export const Hero = () => (
-  <div className={styles.hero}>
+  <>
     <Header />
     <ProjectsList />
-  </div>
+  </>
 );
