5fb487d0c896823cf57dc1683d2fbd31d1d3ea18
iDarkQ <matikuki3@gmail.com>
2026-01-26 08:52:31 +0000

    refactor(lib): remove useless openLinkvertise function

--- apps/web-next/src/shared/lib/utils/index.ts
+++ apps/web-next/src/shared/lib/utils/index.ts
@@ -2,7 +2,7 @@ export { Routes } from "./routes";
 export { capitalizeFirstLetter, extractFirstLinesFromTiptap, singularize } from "./string";
 export { calculateItemDisplayWeight } from "./math";
 export { baseUrl, baseFrontendUrl } from "./url";
-export { getLinkvertiseUrl, openLinkvertise } from "./download";
+export { getLinkvertiseUrl } from "./download";
 export { INPUT_FORMAT } from "./date";
 export { axiosCustomInstance, baseApiConfig } from "./client";
 export type { BedrockComponentProps } from "./components";
