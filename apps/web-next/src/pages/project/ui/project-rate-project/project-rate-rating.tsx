2ef53cc77d445aa9ba1ad21bb77f1f0630f494b7
iDarkQ <matikuki3@gmail.com>
2026-01-28 09:28:33 +0000

    fix(project-rate-project): "Event handlers cannot be passed to Client Component props."

---
+++ apps/web-next/src/pages/project/ui/project-rate-project/project-rate-rating.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { deleteRating, postRating } from "@/entities/project";
+import { Rating } from "@/shared/ui/rating/rating";
+
+import styles from "./project-rate-project.module.scss";
+
+interface ProjectRateRatingProps {
+  id: string;
+  userRating: number;
+}
+
+export const ProjectRateRating = ({
+  id,
+  userRating,
+}: ProjectRateRatingProps) => {
+  const handleReset = async () => {
+    await deleteRating(id);
+  };
+
+  const handleUpdate = async (rating: number) => {
+    await postRating(id, rating);
+  };
+
+  return (
+    <Rating
+      rating={userRating}
+      onReset={handleReset}
+      onUpdate={handleUpdate}
+      className={styles.rating}
+      extraClassName={styles.tooltip}
+      selectable={true}
+      size="medium"
+    />
+  );
+};
