718346f9dbe80f191f760dfd17c420dc60c749cf
AxmBro <axmbro@gmail.com>
2026-01-31 20:36:19 +0100

    fix(author-details): remove unused clsx import and className for simplicity

--- apps/web-next/src/pages/project/ui/project-header/author-details/author-details.tsx
+++ apps/web-next/src/pages/project/ui/project-header/author-details/author-details.tsx
@@ -1,10 +1,7 @@
-import clsx from "clsx";
 import { ProjectMode } from "@/shared/ui/grid-download-card";
 import { DetailedProjectDto } from "@/shared/lib/openapi";
 import { AuthorDetailsAvatar } from "@/pages/project/ui/project-header/author-details/author-details-avatar";
 import { AuthorDetailsSkipButton } from "@/pages/project/ui/project-header/author-details/author-details-skip-button";
-
-import styles from "./author-details.module.scss";
 
 interface AuthorDetails {
   mode: ProjectMode;
@@ -12,7 +9,7 @@ interface AuthorDetails {
 }
 
 export const AuthorDetails = ({ mode, selectedProject }: AuthorDetails) => (
-  <div className={clsx(styles.editor)}>
+  <div>
     <AuthorDetailsAvatar selectedProject={selectedProject} />
     {mode === "view" && <AuthorDetailsSkipButton />}
   </div>
