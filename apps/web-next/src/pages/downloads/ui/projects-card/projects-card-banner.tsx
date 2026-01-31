81a4390a5bd400e5a6f62b4e02db43416e03531b
AxmBro <axmbro@gmail.com>
2026-01-31 23:19:49 +0100

    feat(project-card-banner): implement user check to conditionally display banner (not sure??)

--- apps/web-next/src/pages/downloads/ui/projects-card/projects-card-banner.tsx
+++ apps/web-next/src/pages/downloads/ui/projects-card/projects-card-banner.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import { Banner } from "@/shared/ui/banner";
 import { Routes } from "@/shared/lib/utils";
+import { fetchLoggedUser } from "@/entities/auth";
 
-export const ProjectsCardBanner = () => (
-  <Banner
-    type="info"
-    message={
-      <p style={{ textAlign: "center" }}>
-        Creators keep 100% of ad revenue and benefit from our anti-bypass system
-        to maximize earnings.{" "}
-        <Link
-          href={Routes.MONETIZATION}
-          style={{ color: "inherit", textDecoration: "underline" }}
-        >
-          Check this tutorial to learn how!
-        </Link>
-      </p>
-    }
-  />
-);
+export const ProjectsCardBanner = async (
+) => {
+  const user = await fetchLoggedUser();
+  const shouldShow = user?.linkvertiseId || user?.lootlabsLinkId ? false : true;
+
+  if (!shouldShow) return null;
+
+  return (
+    <Banner
+      type="info"
+      message={
+        <p style={{ textAlign: "center" }}>
+          Creators keep 100% of ad revenue and benefit from our anti-bypass system
+          to maximize earnings.{" "}
+          <Link
+            href={Routes.MONETIZATION}
+            style={{ color: "inherit", textDecoration: "underline" }}
+          >
+            Check this tutorial to learn how!
+          </Link>
+        </p>
+      }
+    />
+  );
+}
