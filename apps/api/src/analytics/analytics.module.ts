0b7bc473aa49e2bec5dac22bda414fe493a6d1b5
iDarkQ <matikuki3@gmail.com>
2026-02-07 18:58:37 +0000

    feat(src): add lootlabs statistics to analytics repsonse

--- apps/api/src/analytics/analytics.module.ts
+++ apps/api/src/analytics/analytics.module.ts
@@ -3,8 +3,10 @@ import { PrismaService } from "~/prisma.service";
 import { AnalyticsService } from "~/analytics/analytics.service";
 import { AnalyticsController } from "~/analytics/analytics.controller";
 import { RatingService } from "~/rating/rating.service";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
+    imports: [HttpModule],
     controllers: [AnalyticsController],
     providers: [AnalyticsService, PrismaService, RatingService],
 })
--- apps/api/src/analytics/analytics.service.ts
+++ apps/api/src/analytics/analytics.service.ts
@@ -1,12 +1,21 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { AnalyticsType } from "@prisma/client";
 import dayjs from "dayjs";
+import { lastValueFrom } from "rxjs";
+import { AnalyticsDto } from "~/analytics/dto/analytics.dto";
+import { LootlabsReportResponseDto } from "~/analytics/dto/lootlabs-report-response.dto";
 import { PrismaService } from "~/prisma.service";
 import { SearchOrder } from "~/project/dto/search-order.dto";
+import { createId } from "@paralleldrive/cuid2";
+import { AnalyticsNames } from "~/analytics/constants/analytics-names";
 
 @Injectable()
 export class AnalyticsService {
-    constructor(private prismaService: PrismaService) {}
+    constructor(
+        private prismaService: PrismaService,
+        private httpService: HttpService,
+    ) {}
 
     async analytics() {
         const topFileProjects = await this.prismaService.analytics.groupBy({
@@ -20,8 +29,8 @@ export class AnalyticsService {
         });
 
         const topFileNames = topFileProjects.map((p) => p.name);
-
-        return this.prismaService.analytics.findMany({
+        const lootlabsData = await this.lootlabsStatistics();
+        const analytics = await this.prismaService.analytics.findMany({
             where: {
                 OR: [
                     { type: { not: "file" } },
@@ -33,6 +42,48 @@ export class AnalyticsService {
             },
             orderBy: { date: "asc" },
         });
+
+        return [...analytics, ...lootlabsData];
+    }
+
+    async lootlabsStatistics() {
+        const token = process.env.MONETIZATION_API_KEY;
+        const method = process.env.MONETIZATION_API_METHOD;
+
+        if (!token || method !== "lootlabs") {
+            return [];
+        }
+
+        try {
+            const response$ = this.httpService.get<LootlabsReportResponseDto>(
+                `https://creators.lootlabs.gg/api/public/reports?api_token=${token}&feeds=1237958&from_date=${dayjs("2026-02-01").format("YYYY-MM-DD")}&to_date=${dayjs().format("YYYY-MM-DD")}&columns=total_revenue,total_impressions&group_by=report_date`,
+            );
+
+            const { data } = await lastValueFrom(response$);
+
+            const analytics: AnalyticsDto[] = [];
+
+            data.message.results.map((result) => {
+                analytics.push({
+                    id: createId(),
+                    type: "general",
+                    date: dayjs(result.report_date).toDate(),
+                    name: AnalyticsNames.adsRevenue,
+                    value: parseFloat((result.total_revenue || 0).toFixed(2)),
+                });
+                analytics.push({
+                    id: createId(),
+                    type: "general",
+                    date: dayjs(result.report_date).toDate(),
+                    name: AnalyticsNames.adsImpressions,
+                    value: result.total_impressions || 0,
+                });
+            });
+
+            return analytics.sort((a, b) => a.date.getTime() - b.date.getTime());
+        } catch (_) {
+            return [];
+        }
     }
 
     async projectAnalytics(projectId: string, search?: SearchOrder) {
