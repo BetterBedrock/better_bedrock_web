97872ac0de4775dbed6aa4045460f9be6a75e548
AxmBro <axmbro@gmail.com>
2026-02-08 18:47:25 +0100

    fix(monetization-page): reorder components to display TermsActions instead of Tutorial

--- apps/web-next/src/pages/monetization/ui/monetization-page.tsx
+++ apps/web-next/src/pages/monetization/ui/monetization-page.tsx
@@ -1,13 +1,15 @@
 import { FAQ } from "./faq/faq";
 import { Hero } from "./hero/hero";
 import { Reasons } from "./reasons/reasons";
+import { TermsActions } from "./terms-actions/terms-actions";
 import { Tutorial } from "./tutorial/tutorial";
 
 export const MonetizationPage = async () => (
   <>
     <Hero />
+    <Tutorial />
     <Reasons />
     <FAQ />
-    <Tutorial />
+    <TermsActions />
   </>
 );
