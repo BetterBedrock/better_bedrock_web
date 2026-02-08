3d3a1d62078ecdee5ce782e79a52e07b308d9bdc
AxmBro <axmbro@gmail.com>
2026-02-08 19:11:50 +0100

    fix(trailer): update text for trailer description and title

--- apps/web-next/src/pages/home/ui/changelog/trailer/trailer-title.tsx
+++ apps/web-next/src/pages/home/ui/changelog/trailer/trailer-title.tsx
@@ -1,5 +1,5 @@
 import { BedrockText } from "@/shared/ui/bedrock-text";
 
 export const TrailerTitle = () => (
-  <BedrockText type="h2" color="white" text="See Better Bedrock in Action" font="Minecraft" />
+  <BedrockText type="h2" color="white" text="See Our Main Texture Pack in Action" font="Minecraft" />
 );
