2ff974836fdcb8fdd3cd1e1a4160275872675f08
AxmBro <axmbro@gmail.com>
2026-02-08 18:50:43 +0100

    feat(terms-actions-buttons): update button styles for Terms and Privacy Policy links

--- apps/web-next/src/pages/advertise/ui/terms-actions/terms-actions-buttons.tsx
+++ apps/web-next/src/pages/advertise/ui/terms-actions/terms-actions-buttons.tsx
@@ -9,13 +9,13 @@ import styles from "./terms-actions.module.scss";
 export const TermsActionsButtons = () => (
     <ButtonGroup direction="responsive">
         <Link href={Routes.TERMS} className={styles.link}>
-            <Button width="100%" type="white" center>
-                <BedrockText text="Terms of Service" type="p" color="black" />
+            <Button width="100%" type="green" center>
+                <BedrockText text="Terms of Service" type="p" color="white" />
             </Button>
         </Link>
         <Link href={Routes.PRIVACY_POLICY} className={styles.link}>
-            <Button width="100%" type="green" center>
-                <BedrockText text="Privacy Policy" type="p" color="white" />
+            <Button width="100%" type="white" center>
+                <BedrockText text="Privacy Policy" type="p" color="black" />
             </Button>
         </Link>
     </ButtonGroup>
