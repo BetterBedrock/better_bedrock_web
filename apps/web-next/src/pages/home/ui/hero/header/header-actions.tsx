246ede32a7d93993bd2b9fb191f0b98091602239
iDarkQ <matikuki3@gmail.com>
2026-02-14 10:21:17 +0000

    refactor(header-actions.tsx): change projects to bedrock mods

--- apps/web-next/src/pages/home/ui/hero/header/header-actions.tsx
+++ apps/web-next/src/pages/home/ui/hero/header/header-actions.tsx
@@ -11,7 +11,7 @@ export const HeroActions = () => (
     <ButtonGroup direction="responsive">
       <Link href={Routes.DOWNLOADS_MAIN} className={styles.link}>
         <Button width="100%" type="green" center>
-          <BedrockText text="Explore Projects" type="p" color="white" />
+          <BedrockText text="Explore Bedrock Mods" type="p" color="white" />
         </Button>
       </Link>
       <Link href={Routes.DOWNLOADS_BETTERBEDROCK} className={styles.link}>
