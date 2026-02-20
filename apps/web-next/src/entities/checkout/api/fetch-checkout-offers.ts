ebcf44fbc100eb25e2fce756584e01b6483e00dd
iDarkQ <matikuki3@gmail.com>
2026-02-20 08:49:28 +0000

    refactor(web-next): simplify fetch checkout offers

--- apps/web-next/src/entities/checkout/api/fetch-checkout-offers.ts
+++
@@ -1,6 +0,0 @@
-import { fetchCheckoutOffersRequest } from "@/entities/checkout/api/checkout-service";
-
-export const fetchCheckoutOffers = async () => {
-    const { data } = await fetchCheckoutOffersRequest();
-    return data;
-};
