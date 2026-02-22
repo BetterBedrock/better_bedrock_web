4a3b45f5cf863117916199eb3669b9ad0ee96c20
iDarkQ <matikuki3@gmail.com>
2026-02-22 06:18:11 +0000

    refactor(layout.tsx): remove generic downloads page description

--- apps/web-next/app/(projects)/downloads/layout.tsx
+++ apps/web-next/app/(projects)/downloads/layout.tsx
@@ -1,17 +1,11 @@
 import { Section } from "@/shared/ui/section";
 import { ReactNode } from "react";
-import type { Metadata } from "next";
 
 import { styles } from "@/pages/downloads";
 
 interface DownloadsLayoutProps {
   children: ReactNode;
 }
-
-export const metadata: Metadata = {
-  title:
-    "Minecraft Bedrock Mods, Texture Packs, Maps, Skins & More | Better Bedrock",
-};
 
 export default function DownloadsLayout({ children }: DownloadsLayoutProps) {
   return (
