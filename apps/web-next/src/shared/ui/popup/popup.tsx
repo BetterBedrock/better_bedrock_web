310d561d3624d3403ceedda6d6b14cfe05da8431
AxmBro <axmbro@gmail.com>
2026-02-28 17:08:28 +0100

    feat(hooks): add useFocusTrap hook for managing focus within elements

--- apps/web-next/src/shared/ui/popup/popup.tsx
+++ apps/web-next/src/shared/ui/popup/popup.tsx
@@ -2,7 +2,7 @@ import Exit from "@/public/images/exit.png";
 import { Children, FC, Fragment, HTMLAttributes, ReactNode, useEffect, useRef } from "react";
 import { BedrockText } from "@/shared/ui/bedrock-text";
 import { SimpleButton } from "@/shared/ui/simple-button";
-import { useFocusTrap } from "@/shared/hooks/useFocusTrap";
+import { useFocusTrap } from "@/shared/lib/hooks";
 
 import styles from "./popup.module.scss";
 import { Card } from "@/shared/ui/card";
