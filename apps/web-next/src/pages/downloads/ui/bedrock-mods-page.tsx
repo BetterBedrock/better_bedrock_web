5dc7ad776320198adb51d105dcc57b9bd523ae55
iDarkQ <matikuki3@gmail.com>
2026-03-05 16:18:21 +0000

    refactor(bedrock-mods-page.tsx): hide featuredcard if page > 1

--- apps/web-next/src/pages/downloads/ui/bedrock-mods-page.tsx
+++ apps/web-next/src/pages/downloads/ui/bedrock-mods-page.tsx
@@ -1,14 +1,19 @@
 import { FeaturedCard } from "@/pages/downloads/ui/featured-card/featured-card";
 import { ProjectsCard } from "@/pages/downloads/ui/projects-card/projects-card";
+import { getCurrentPaginationPage } from "@/shared/lib/utils/math";
 
 export interface BedrockModsProps {
   params: Promise<{ page?: string[] }>;
 }
 
-export const BedrockModsPage = async ({ params }: BedrockModsProps) => (
-  <>
-    <FeaturedCard />
-    {/* <ContentCard /> */}
-    <ProjectsCard params={params} />
-  </>
-);
+export const BedrockModsPage = async ({ params }: BedrockModsProps) => {
+  const { page } = await params;
+  const currentPage = getCurrentPaginationPage(page);
+
+  return (
+    <>
+      {currentPage <= 1 && <FeaturedCard />}
+      <ProjectsCard page={currentPage} />
+    </>
+  );
+};
