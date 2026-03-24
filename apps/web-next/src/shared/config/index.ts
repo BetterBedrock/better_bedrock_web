beed74948d3659ea547dba5c54fd64f36aabdb78
iDarkQ <matikuki3@gmail.com>
2026-03-24 14:30:52 +0000

    refactor(utils): remove getCurrentPaginationPage function

--- apps/web-next/src/shared/config/index.ts
+++ apps/web-next/src/shared/config/index.ts
@@ -1,4 +1,6 @@
-export { SEARCH_PROJECT_TYPES, PROJECT_TYPES, SIDE_PROJECTS_LIST, MAIN_LIST } from "./better-bedrock";
+export { SEARCH_PROJECT_TYPES, PROJECT_TYPES, SIDE_PROJECTS_LIST, MAIN_LIST, urlToProjectsType, bedrockDownloadPages } from "./better-bedrock";
+export type { BedrockDownloadPage } from "./better-bedrock";
+export type { UrlToProjectsTypeKeys } from "./better-bedrock";
 export type { DownloadsCategoryDto } from "./dto/downloads-category.dto";
 export type { DownloadsItemDto, DownloadsItemTypeKey } from "./dto/downloads-item.dto";
 export type { DownloadsListDto } from "./dto/downloads-list.dto";
