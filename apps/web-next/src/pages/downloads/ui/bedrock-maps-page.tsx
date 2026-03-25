117a1f8e66be0e1632a8e5e7b09747d1ce8fb6f9
iDarkQ <matikuki3@gmail.com>
2026-03-25 14:31:55 +0000

    refactor(downloads): redirect all traffic from bedrock-* to one bedrock-page.tsx

--- apps/web-next/src/pages/downloads/ui/bedrock-maps-page.tsx
+++ apps/web-next/src/pages/downloads/ui/bedrock-maps-page.tsx
@@ -1,19 +1,6 @@
-import { FeaturedCard } from "@/pages/downloads/ui/featured-card/featured-card";
-import { ProjectsCard } from "@/pages/downloads/ui/projects-card/projects-card";
-import { getCurrentPaginationPage } from "@/shared/lib/utils";
+import { Routes } from "@/shared/lib/utils";
+import { permanentRedirect } from "next/navigation";
 
-export interface BedrockMapsPageProps {
-  params: Promise<{ page?: string[] }>;
-}
-
-export const BedrockMapsPage = async ({ params }: BedrockMapsPageProps) => {
-  const { page } = await params;
-  const currentPage = getCurrentPaginationPage(page);
-
-  return (
-    <>
-      {currentPage <= 1 && <FeaturedCard />}
-      <ProjectsCard type="maps" page={currentPage} />
-    </>
-  );
+export const BedrockMapsPage = async () => {
+  permanentRedirect(Routes.DOWNLOADS_BEDROCK_MAPS);
 };
--- apps/web-next/src/pages/downloads/ui/bedrock-skin-packs-page.tsx
+++ apps/web-next/src/pages/downloads/ui/bedrock-skin-packs-page.tsx
@@ -1,19 +1,6 @@
-import { FeaturedCard } from "@/pages/downloads/ui/featured-card/featured-card";
-import { ProjectsCard } from "@/pages/downloads/ui/projects-card/projects-card";
-import { getCurrentPaginationPage } from "@/shared/lib/utils";
+import { Routes } from "@/shared/lib/utils";
+import { permanentRedirect } from "next/navigation";
 
-export interface BedrockSkinPacksPageProps {
-  params: Promise<{ page?: string[] }>;
-}
-
-export const BedrockSkinPacksPage = async ({ params }: BedrockSkinPacksPageProps) => {
-  const { page } = await params;
-  const currentPage = getCurrentPaginationPage(page);
-
-  return (
-    <>
-      {currentPage <= 1 && <FeaturedCard />}
-      <ProjectsCard type="skinPacks" page={currentPage} />
-    </>
-  );
+export const BedrockSkinPacksPage = async () => {
+  permanentRedirect(Routes.DOWNLOADS_BEDROCK_SKIN_PACKS);
 };
