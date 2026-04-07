201f1e82f0918960905bd55148b506825835908b
iDarkQ <matikuki3@gmail.com>
2026-04-07 15:28:53 +0100

    refactor(index.ts): export fetchLiveCount

--- apps/web-next/src/entities/analytic/index.ts
+++ apps/web-next/src/entities/analytic/index.ts
@@ -1,3 +1,4 @@
 export { fetchUserAnalytics } from "./api/fetch-user-analytics";
 export { fetchAllAnalytics } from "./api/fetch-all-analytics";
+export { fetchLiveCount } from "./api/fetch-live-count";
 export { simplifyAnalytics } from "./lib/simplify-analytics";
