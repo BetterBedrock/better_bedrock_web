332d2c157582a7e3ae8b00837a791ff3a3136371
iDarkQ <matikuki3@gmail.com>
2026-04-15 08:24:09 +0100

    refactor: delete downloads/main page

--- apps/web-next/src/pages/downloads/ui/main-page.tsx
+++
@@ -1,6 +0,0 @@
-import { Routes } from "@/shared/lib/utils";
-import { permanentRedirect } from "next/navigation";
-
-export const MainPage = async () => {
-  permanentRedirect(Routes.DOWNLOADS_BEDROCK_MODS);
-}
