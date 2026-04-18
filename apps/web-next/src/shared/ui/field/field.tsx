5e996711bcd2750001929b7178f3edce1801f7a0
iDarkQ <matikuki3@gmail.com>
2026-04-18 08:38:46 +0100

    refactor(field.tsx): require only error for the right field

--- apps/web-next/src/shared/ui/field/field.tsx
+++ apps/web-next/src/shared/ui/field/field.tsx
@@ -1,27 +1,26 @@
 import { BedrockText } from "@/shared/ui/bedrock-text";
 import { ReactNode } from "react";
-import { FieldErrors } from "react-hook-form";
+import { FieldError } from "react-hook-form";
 
 import styles from "./field.module.scss";
 
 interface FieldProps {
   name: string;
-  id: string;
-  errors: FieldErrors;
+  error: FieldError | undefined;
   children: ReactNode;
 }
 
-export const Field = ({ name, id, errors, children }: FieldProps) => (
+export const Field = ({ name, error, children }: FieldProps) => (
   <>
     <BedrockText textAlign="start" color="white">
       {name}
     </BedrockText>
     {children}
-    {errors[id] && (
+    {error?.message && (
       <BedrockText
         type="p2"
         extraClassName={styles.error}
-        text={errors[id].message as string}
+        text={error.message}
         textAlign="start"
       />
     )}
