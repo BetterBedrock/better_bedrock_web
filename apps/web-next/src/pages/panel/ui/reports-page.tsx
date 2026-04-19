2316c020498ee8cd0ff67e06bf62f2c48a0fbcf7
iDarkQ <matikuki3@gmail.com>
2026-04-19 08:41:55 +0100

    refactor(reports-page.tsx): move all logic to interal components & leave only HeroReports

--- apps/web-next/src/pages/panel/ui/reports-page.tsx
+++ apps/web-next/src/pages/panel/ui/reports-page.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import { useReportsManager } from "@/pages/panel/model/reports-manager";
 import { HeroReports } from "@/pages/panel/ui/hero-reports/hero-reports";
-import { HeroReportsPopup } from "@/pages/panel/ui/hero-reports/hero-reports-popup/hero-reports-popup";
 
-export const ReportsPage = () => {
-  const { selectedReport } = useReportsManager();
-
-  return (
-    <>
-      {selectedReport && <HeroReportsPopup />}
-      <HeroReports />
-    </>
-  );
-};
+export const ReportsPage = () => <HeroReports />;
